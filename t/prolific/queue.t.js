be15747ee46222764d215033d44022748d6370ea
Alan Gutierrez <alan@prettyrobots.com>
2015-07-21 14:30:21 -0500

    Implement logging queue.

---
+++ t/prolific/queue.t.js
@@ -0,0 +1,16 @@
+require('proof')(1, require('cadence/redux')(prove))
+
+function prove (async, assert) {
+    var stream = require('stream')
+    var Queue = require('../../queue')
+    var out = new stream.PassThrough
+    var queue = new Queue(out)
+    async(function () {
+        queue.write(1)
+        queue.write(2)
+        queue.write(3)
+        queue.flush(async())
+    }, function () {
+        assert(out.read().toString(), '123', 'written')
+    })
+}
