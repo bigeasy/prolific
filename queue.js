cb1f2ce02e9bd2bf51b6653bec2d22aae9a07e7a
Alan Gutierrez <alan@prettyrobots.com>
2015-10-29 12:12:46 -0400

    Implement crash freindly queue.
    Closes #13.

--- queue.js
+++ queue.js
@@ -1,19 +1,22 @@
-var Staccato = require('staccato')
 var cadence = require('cadence')
 
-function Queue (out) {
-    this._entries = []
-    this.out = new Staccato(out, false)
+function Queue () {
+    this._entries = [[]]
 }
 
-Queue.prototype.flush = cadence(function (async) {
-    var blob = this._entries.join('')
-    this._entries.length = 0
-    this.out.write(blob, async())
+Queue.prototype.flush = cadence(function (async, out) {
+    if (this._entries.length == 1) {
+        this._entries.unshift([])
+    }
+    async(function () {
+        out.write(this._entries[this._entries.length - 1].join(''), async())
+    }, function () {
+        this._entries.pop()
+    })
 })
 
 Queue.prototype.write = function (line) {
-    this._entries.push(line)
+    this._entries[0].push(line)
 }
 
 module.exports = Queue
--- t/prolific/queue.t.js
+++ t/prolific/queue.t.js
@@ -4,12 +4,13 @@ function prove (async, assert) {
     var stream = require('stream')
     var Queue = require('../../queue')
     var out = new stream.PassThrough
-    var queue = new Queue(out)
+    var queue = new Queue()
     async(function () {
         queue.write(1)
         queue.write(2)
         queue.write(3)
-        queue.flush(async())
+        queue.flush({ write: function () {} }, function () {})
+        queue.flush(out, async())
     }, function () {
         assert(out.read().toString(), '123', 'written')
     })
