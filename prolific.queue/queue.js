df59e26485fa6cde5045e594912518f35cde5def
Alan Gutierrez <alan@prettyrobots.com>
2016-05-23 12:38:02 -0500

    Remove `pid` from `Chunk`.

--- prolific.queue/queue.js
+++ prolific.queue/queue.js
@@ -1,14 +1,13 @@
 var cadence = require('cadence')
 var Chunk = require('prolific.chunk')
 
-function Queue (pid, stream) {
-    this._pid = pid
+function Queue (stream) {
     this._entries = []
     this._chunks = []
     this._chunkNumber = 1
     this._previousChecksum = 'aaaaaaaa'
     this._stream = stream
-    this._chunks.push(new Chunk('x', 0, new Buffer(''), 1))
+    this._chunks.push(new Chunk(0, new Buffer(''), 1))
 }
 
 Queue.prototype.write = function (line) {
@@ -23,7 +22,7 @@ Queue.prototype._chunkEntries = function () {
     this._entries = []
 
     var buffer = new Buffer(entries.join(''))
-    this._chunks.push(new Chunk('x', this._chunkNumber++, buffer, buffer.length))
+    this._chunks.push(new Chunk(this._chunkNumber++, buffer, buffer.length))
 }
 
 Queue.prototype.flush = cadence(function (async) {
@@ -63,7 +62,7 @@ Queue.prototype.exit = function (stderr) {
         return
     }
 
-    this._chunks.unshift(new Chunk('x', 0, new Buffer(''), this._chunks[0].number))
+    this._chunks.unshift(new Chunk(0, new Buffer(''), this._chunks[0].number))
 
     var buffers = []
     while (this._chunks.length) {
