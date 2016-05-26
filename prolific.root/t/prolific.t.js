a32b4a8593c059df482df71ca4b1d1308ced66b0
Alan Gutierrez <alan@prettyrobots.com>
2016-05-26 01:37:51 -0500

    Find Prolific Main on require.

--- prolific.root/t/prolific.t.js
+++ prolific.root/t/prolific.t.js
@@ -1,24 +1,6 @@
-require('proof')(7, prove)
+require('proof')(1, prove)
 
 function prove (assert) {
     var prolific = require('..')
-    prolific._Date = { now: function () { return 0 } }
-    prolific.sink.write()
-    prolific.sink = {
-        write: function (line) {
-            assert(line, '{"a":1}\n', 'write')
-        }
-    }
-    prolific._write({ a: 1 })
-    prolific.setLevel('hello.world', 'trace')
-    assert(prolific._supersede.get([ '', 'hello' ]), 'info', 'get default')
-    assert(prolific._supersede.get([ '', 'hello', 'world' ]), 'trace', 'get')
-    prolific.setLevel('debug')
-    assert(prolific._supersede.get([ '', 'hello' ]), 'debug', 'set default')
-    prolific.clearLevel('hello.world')
-    assert(prolific._supersede.get([ '', 'hello', 'world' ]), 'debug', 'clear')
-    prolific.clearLevel()
-    assert(prolific._supersede.get([ '', 'hello', 'world' ]), 'info', 'clear default')
-
-    assert(prolific.createLogger('hello.world'), 'create logger')
+    assert(prolific.sink, 'required')
 }
