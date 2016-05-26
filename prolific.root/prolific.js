16d8d68734ee7c8b4535c2036373ab6557a77c3a
Alan Gutierrez <alan@prettyrobots.com>
2016-05-26 02:00:57 -0500

    Fallback if there is no Prolific Main in `main`.

--- prolific.root/prolific.js
+++ prolific.root/prolific.js
@@ -1,1 +1,6 @@
-module.exports = require.main.require('prolific.main')
+try {
+    module.exports = require.main.require('prolific.main')
+} catch (e) {
+    module.exports = require('prolific.main')
+    module.exports.setLevel('error')
+}
