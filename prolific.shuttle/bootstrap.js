3a7442627487fb338f1bb7e5e6d920829dafc0a3
Alan Gutierrez <alan@prettyrobots.com>
2016-06-11 14:10:58 -0400

    Shuttle with optional initialization.

--- prolific.shuttle/bootstrap.js
+++ prolific.shuttle/bootstrap.js
@@ -3,13 +3,17 @@ var abend = require('abend')
 var Isochronous = require('isochronous')
 
 exports.createShuttle = function (net, Shuttle) {
-    return function (program, interval, configuration, finale) {
+    return function (program, interval, initialization, finale) {
+        if (arguments.length == 3) {
+            finale = initialization
+            initialization = {}
+        }
         if (program.env.PROLIFIC_CONFIGURATION != null) {
 // TODO Maybe delete and internalize?
             var configuration = JSON.parse(program.env.PROLIFIC_CONFIGURATION)
             var pipe = new net.Socket({ fd: configuration.fd  })
             var shuttle = new Shuttle(pipe, pipe, program.stderr, finale)
-            shuttle.queue.write(new Buffer(JSON.stringify(configuration) + '\n'))
+            shuttle.queue.write(new Buffer(JSON.stringify(initialization) + '\n'))
             prolific.sink = shuttle.queue
             program.on('uncaughtException', shuttle.uncaughtException.bind(shuttle))
             program.on('exit', shuttle.stop.bind(shuttle))
--- prolific.shuttle/t/bootstrap.t.js
+++ prolific.shuttle/t/bootstrap.t.js
@@ -1,21 +1,35 @@
-require('proof')(1, prove)
+require('proof')(4, prove)
 
-function prove (assert) {
+function test (assert, prefix, written) {
     var events = require('events')
     var bootstrap = require('../bootstrap')
     var net = {
         Socket: function (options) {
-            assert(options, { fd: 3 }, 'socket')
+            assert(options, { fd: 3 }, prefix + 'socket')
         }
     }
     function Shuttle () {
-        this.queue = { write: function () {} }
+        this.queue = {
+            write: function (value) {
+                assert(value.toString(), written, prefix + 'init')
+            }
+        }
         this.uncaughtException = function () {}
         this.stop = function () {}
     }
-    var program = new events.EventEmitter
     var createShuttle = bootstrap.createShuttle(net, Shuttle)
     createShuttle({ env: {} })
+    return createShuttle
+}
+
+function prove (assert) {
+    var createShuttle
+    var events = require('events')
+    var program = new events.EventEmitter
     program.env = { PROLIFIC_CONFIGURATION: JSON.stringify({ fd: 3 }) }
-    createShuttle(program, 1000, {}, function () {})
+    createShuttle = test(assert, '', '{"a":1}\n')
+    createShuttle({ env: {} })
+    createShuttle(program, 1000, { a: 1 }, function () {})
+    createShuttle = test(assert, 'no init ', '{}\n')
+    createShuttle(program, 1000, function () {})
 }
