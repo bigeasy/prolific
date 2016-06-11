2665d54cbae2ce245bdde676975cd95a2b952f94
Alan Gutierrez <alan@prettyrobots.com>
2016-06-11 14:11:42 -0400

    Rename `initialization` to `parameters`.

--- prolific.shuttle/bootstrap.js
+++ prolific.shuttle/bootstrap.js
@@ -3,17 +3,17 @@ var abend = require('abend')
 var Isochronous = require('isochronous')
 
 exports.createShuttle = function (net, Shuttle) {
-    return function (program, interval, initialization, finale) {
+    return function (program, interval, parameters, finale) {
         if (arguments.length == 3) {
-            finale = initialization
-            initialization = {}
+            finale = parameters
+            parameters = {}
         }
         if (program.env.PROLIFIC_CONFIGURATION != null) {
 // TODO Maybe delete and internalize?
             var configuration = JSON.parse(program.env.PROLIFIC_CONFIGURATION)
             var pipe = new net.Socket({ fd: configuration.fd  })
             var shuttle = new Shuttle(pipe, pipe, program.stderr, finale)
-            shuttle.queue.write(new Buffer(JSON.stringify(initialization) + '\n'))
+            shuttle.queue.write(new Buffer(JSON.stringify(parameters) + '\n'))
             prolific.sink = shuttle.queue
             program.on('uncaughtException', shuttle.uncaughtException.bind(shuttle))
             program.on('exit', shuttle.stop.bind(shuttle))
