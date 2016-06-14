72ad0ab254ee29c4f1f2096a0352e065209e3b27
Alan Gutierrez <alan@prettyrobots.com>
2016-06-14 10:39:36 -0500

    Stop Isochronous on stop.

--- prolific.shuttle/shuttle.js
+++ prolific.shuttle/shuttle.js
@@ -7,8 +7,6 @@ function Shuttle (input, output, sync, uncaught, interval) {
     this.input = input
     this.output = output
     this.queue = new Queue(output)
-    this._terminated = false
-    this.stopped = false
     this.sync = sync
     this.uncaught = createUncaughtExceptionHandler(uncaught)
 // TODO Interval is so dubious, why not just write?
@@ -27,15 +25,12 @@ Shuttle.prototype.uncaughtException = function (error) {
 
 Shuttle.prototype.stop = function () {
     this.queue.close()
+    this.isochronous.stop()
 }
 
 Shuttle.prototype.exit = function () {
-    if (!this._terminated) {
-        this._terminated = true
-        this.stop()
-        this.isochronous.stop()
-        this.queue.exit(this.sync)
-    }
+    this.stop()
+    this.queue.exit(this.sync)
 }
 
 Shuttle.shuttle = require('./bootstrap').createShuttle(require('net'), Shuttle)
