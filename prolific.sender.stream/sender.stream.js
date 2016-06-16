96655d7be897006964c189d84b5a581fc0f5c73e
Alan Gutierrez <alan@prettyrobots.com>
2016-06-15 19:39:47 -0500

    Base stream writer for processors.

--- prolific.sender.stream/sender.stream.js
+++ prolific.sender.stream/sender.stream.js
@@ -1,28 +1,32 @@
 var cadence = require('cadence')
-var Reactor = require('reactor')
-var fs = require('fs')
+var abend = require('abend')
 
-function Processor (stream, close) {
-    this._sending = new Reactor({ object: this, method: '_send' })
+function Processor (stream) {
     this._stream = stream
-    this._close = close
+    this._sending = false
+    this.lines = []
 }
 
-Processor.prototype.send = function (chunk, callback) {
-    this._sending.push(chunk, callback || null)
+Processor.prototype.process = function (entry) {
+    this.lines.push(entry.formatted || JSON.stringify(entry) + '\n')
+    if (!this._sending) {
+        this._send(abend)
+    }
 }
 
-Processor.prototype._send = cadence(function (async, timeout, chunk) {
-    this._stream.write(chunk.buffer, async())
-})
-
-Processor.prototype.close = cadence(function (async) {
-    async(function () {
-        this.send({ buffer: new Buffer(0) }, async())
-    }, function () {
-        if (this._close) {
-            this._stream.end(async())
-        }
+Processor.prototype._send = cadence(function (async) {
+    async([function () {
+        this._sending = false
+    }], function () {
+        this._sending = true
+        var loop = async(function () {
+            if (this.lines.length == 0) {
+                return [ loop.break ]
+            }
+            var lines = this.lines
+            this.lines = []
+            this._stream.write(lines.join(''), async())
+        })()
     })
 })
 
--- prolific.sender.stream/t/sender.stream.t.js
+++ prolific.sender.stream/t/sender.stream.t.js
@@ -4,23 +4,20 @@ function prove (async, assert) {
     var Sender = require('..')
     var stream = require('stream')
 
-    async(function () {
-        var out = new stream.PassThrough
-        var sender = new Sender(out, true)
-        async(function () {
-            sender.send({ buffer: new Buffer('a\n') })
-            sender.close(async())
-        }, function () {
-            assert(out.read().toString(), 'a\n', 'closed')
-        })
-    }, function () {
-        var out = new stream.PassThrough
-        var sender = new Sender(out, false)
-        async(function () {
-            sender.send({ buffer: new Buffer('a\n') })
-            sender.close(async())
-        }, function () {
-            assert(out.read().toString(), 'a\n', 'closed')
-        })
+    var wait = async()
+    var out = new stream.PassThrough
+    var sender = new Sender(out)
+    sender.process({ formatted: 'foo\n' })
+    sender.process({ a: 1 })
+    var expected = [
+        { name: 'formatted', value: 'foo\n' },
+        { name: 'entry', value: '{"a":1}\n' }
+    ]
+    out.on('data', function (data) {
+        var expect = expected.shift()
+        assert(data.toString(), expect.value, expect.name)
+        if (expected.length == 0) {
+            wait()
+        }
     })
 }
