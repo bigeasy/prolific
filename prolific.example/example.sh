81a8eb6e1bdea2f6b2adab21d935702d34555504
Alan Gutierrez <alan@prettyrobots.com>
2016-06-16 14:00:30 -0500

    Thoughts on `filter`.

--- prolific.example/example.sh
+++ prolific.example/example.sh
@@ -5,14 +5,15 @@ prolific run -- node program.bin.js
 
 prolific run \
     tcp --url tcp://127.0.0.1:8514 \
-    syslog --appliation foo --serialize wafer \
-    tee \
+    syslog --application foo --serialize wafer \
+  tee \
     tcp --url tcp://127.0.0.1:514 \
     node parent.bin.js --param value prolific run node child.bin.js
 
 prolific \
-        tcp://127.0.0.1:8514 \
-        syslog --appliation foo --serialize wafer \
-    tee \
-        tcp://127.0.0.1:514 \
-    node parent.bin.js --param value prolific --configuration inherit node child.bin.js
+    tcp://127.0.0.1:8514 \
+    syslog --application paxos --serialize wafer \
+  tee \
+    tcp://127.0.0.1:514 \
+    filter --match '$.context[3] == "bigeasy.paxos" && $level <= $trace' \
+  node parent.bin.js --param value prolific --configuration inherit node child.bin.js
