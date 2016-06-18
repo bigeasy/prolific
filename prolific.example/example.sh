13e4d43a0d4ef21b94ccf941449f94efc98e98ce
Alan Gutierrez <alan@prettyrobots.com>
2016-06-18 14:20:10 -0500

    Thoughts on loading modules.

--- prolific.example/example.sh
+++ prolific.example/example.sh
@@ -10,10 +10,13 @@ prolific run \
     tcp --url tcp://127.0.0.1:514 \
     node parent.bin.js --param value prolific run node child.bin.js
 
+# TODO Some sort of language to include modules, so I can stop poluting the name
+# space with shortly-thereafter renames.
 prolific \
     tcp://127.0.0.1:8514 \
     syslog --application paxos --serialize wafer \
   tee \
     tcp://127.0.0.1:514 \
-    filter --match '$.context[3] == "bigeasy.paxos" && $level <= $trace' \
+    @prolific.monitor/filter +'$context[3] == "bigeasy.paxos" && $level <= $trace' \
+    aggregate --with 'bigeasy.service:health' --avg 'http=$.http.mean' --sum 'messages=$.messages' \
   node parent.bin.js --param value prolific --configuration inherit node child.bin.js
