a4e548ff14f406fbce78203908a1022cf0d11983
Alan Gutierrez <alan@prettyrobots.com>
2016-06-22 10:38:31 -0500

    No more fancy printing of uncaught errors.

--- prolific.shuttle/t/uncaught.t.js
+++ prolific.shuttle/t/uncaught.t.js
@@ -1,23 +1,15 @@
-require('proof')(2, prove)
+require('proof')(3, prove)
 
 function prove (assert) {
     var createUncaughtExceptionHandler = require('../uncaught')
     createUncaughtExceptionHandler(function (error) {
         assert(error.message, 'function', 'function handler')
     })(new Error('function'))
-    var cause = new Error('cause')
-    var base = { ignore: true }
-    var error = Object.create(base)
-    error.stack = true
-    error.code = 1
-    error.cause = cause
+    var error = new Error('handled')
     createUncaughtExceptionHandler({
         error: function (message, properties) {
-            properties.stack = !! properties.stack
-            properties.cause.stack = !! properties.cause.stack
-            assert(properties, {
-                stack: true, code: 1, cause: { stack: true }
-            }, 'logger handler')
+            assert(message, 'uncaught', 'message')
+            assert(properties.stack, error.stack, 'stack')
         }
     })(error)
 }
