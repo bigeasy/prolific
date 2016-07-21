663f32fa0f5b577b89b0d2d8d7a11afca9765438
Alan Gutierrez <alan@prettyrobots.com>
2016-07-21 14:43:57 -0500

    Specify logger with URL.

--- prolific.monitor/programmatic.js
+++ prolific.monitor/programmatic.js
@@ -2,9 +2,15 @@ module.exports = function (program, terminal, argv) {
     if (argv.length == 0) {
         program.abend('no program')
     }
-    return terminal || !/^[\w.]+$/.test(argv[0]) || argv[0] == 'node' || (function () {
+    return terminal || !/(^[\w.]+$|^[\w.]+:)/.test(argv[0]) || argv[0] == 'node' || (function () {
         try {
-            return ! require('prolific.' + argv[0] + '/__prolific__').isProlific
+            var command = argv[0]
+            if (~command.indexOf(':')) {
+                argv.shift()
+                argv.unshift(command.split(':')[0], '--url', command)
+            }
+            command = argv[0]
+            return ! require('prolific.' + command + '/__prolific__').isProlific
         } catch (e) {
             return true
         }
--- prolific.monitor/t/programmatic.t.js
+++ prolific.monitor/t/programmatic.t.js
@@ -1,4 +1,4 @@
-require('proof')(2, prove)
+require('proof')(5, prove)
 
 function prove (assert) {
     var programmatic = require('../programmatic')
@@ -8,4 +8,8 @@ function prove (assert) {
         }
     }, null, [])
     assert(programmatic(null, false, [ 'echo' ]), 'no module found')
+    assert(!programmatic(null, false, [ 'test' ]), 'test found')
+    var argv = [ 'test://127.0.0.1' ]
+    assert(!programmatic(null, false, argv), 'test url found')
+    assert(argv, [ 'test', '--url', 'test://127.0.0.1' ], 'url expanded')
 }
