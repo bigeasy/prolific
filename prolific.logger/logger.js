e53792093e48f6ed3587722c52fc6bfca04f3dba
Alan Gutierrez <alan@prettyrobots.com>
2016-07-28 10:09:07 -0500

    Log method expected by Inlet.

--- prolific.logger/logger.js
+++ prolific.logger/logger.js
@@ -3,6 +3,11 @@ var prolific = require('prolific')
 function Logger (qualifier) {
     this.qualifier = qualifier
     this._path = ('.' + qualifier).split('.')
+    this.log = this._log.bind(this)
+}
+
+Logger.prototype._log = function (level, name, properties) {
+    prolific.json(this._path, level, this.qualifier, name, properties)
 }
 
 ; [ 'error', 'warn', 'info', 'debug', 'trace' ].forEach(function (level) {
