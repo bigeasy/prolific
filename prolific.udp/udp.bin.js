14e5b9f89b7d6b7fcc2cb26ef4e7e52bca46a0fb
Alan Gutierrez <alan@prettyrobots.com>
2016-07-28 07:34:10 -0500

    Add UDP listener.

---
+++ prolific.udp/udp.bin.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/*
+    ___ usage ___ en_US ___
+    prolific.udp --bind ip:port
+
+    options:
+
+      --help
+          display help message
+
+      -b, --bind  <address:port>
+          port to listen to
+
+    ___ . ___
+
+ */
+require('arguable')(module, require('cadence')(function (async, program) {
+    program.helpIf(program.param.help)
+    program.command.required('bind')
+
+    var logger = require('prolific.logger').createLogger('prolific.udp.listener')
+    Shuttle.shuttle(program, 1000, logger)
+
+    var dgram = require('dgram')
+    var socket = dgram.createSocket('udp4')
+    socket.on('message', function (buffer) {
+        prolific.sink.write(chunk)
+    })
+    program.on('SIGINT', socket.close.bind(socket))
+    program.on('SIGTERM', socket.close.bind(socket))
+    socket.bind(+program.param.port, async())
+}))
