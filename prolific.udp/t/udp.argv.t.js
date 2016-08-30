a5a3c5bd7157a3adfd017ce72137f5d709b61720
Alan Gutierrez <alan@prettyrobots.com>
2016-08-30 08:51:38 -0500

    Remove unused argument.

--- prolific.udp/t/udp.argv.t.js
+++ prolific.udp/t/udp.argv.t.js
@@ -3,7 +3,7 @@ require('proof/redux')(2, require('cadence')(prove))
 function prove (async, assert) {
     var argv = require('../udp.argv'), program
     async(function () {
-        argv([ '--url', 'udp://127.0.0.1:514' ], {}, async())
+        argv([ '--url', 'udp://127.0.0.1:514' ], async())
     }, function (result) {
         assert(result, {
             moduleName: 'prolific.udp/udp.processor',
