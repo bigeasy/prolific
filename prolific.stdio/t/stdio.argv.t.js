aa7df29a95dcbf7782879a723ec48189ce2375b9
Alan Gutierrez <alan@prettyrobots.com>
2016-08-30 09:05:54 -0500

    Remove unused argument.

--- prolific.stdio/t/stdio.argv.t.js
+++ prolific.stdio/t/stdio.argv.t.js
@@ -3,7 +3,7 @@ require('proof/redux')(1, require('cadence')(prove))
 function prove (async, assert) {
     var argv = require('../stdio.argv'), program
     async(function () {
-        argv([ '--stderr' ], {}, async())
+        argv([ '--stderr' ], async())
     }, function (result) {
         assert(result, {
             moduleName: 'prolific.stdio/stdio.processor',
