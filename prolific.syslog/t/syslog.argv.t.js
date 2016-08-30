4bfac2d637df1435a84dcb0d43f62f2fdd698d6b
Alan Gutierrez <alan@prettyrobots.com>
2016-08-30 10:49:12 -0500

    Fix Syslog argument parser test count.

--- prolific.syslog/t/syslog.argv.t.js
+++ prolific.syslog/t/syslog.argv.t.js
@@ -1,4 +1,4 @@
-require('proof/redux')(2, require('cadence')(prove))
+require('proof/redux')(1, require('cadence')(prove))
 
 function prove (async, assert) {
     var argv = require('../syslog.argv'), program
