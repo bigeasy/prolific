4dde1e7b5dfee18c3ff6f4c24de223869b53f0e0
Alan Gutierrez <alan@prettyrobots.com>
2016-08-30 09:09:13 -0500

    Remove argument printout.

--- prolific.syslog/syslog.argv.js
+++ prolific.syslog/syslog.argv.js
@@ -29,16 +29,12 @@
 
 require('arguable')(module, require('cadence')(function (async, program) {
     program.helpIf(program.ultimate.help)
-    var response = {
+    return  {
         moduleName: 'prolific.syslog/syslog.processor',
         parameters: program.ultimate,
         argv: program.argv,
         terminal: program.terminal
     }
-    if (program.isMainModule) {
-        program.stdout.write(require('util').inspect(response, { depth: null }) + '\n')
-    }
-    return response
 }))
 
 module.exports.isProlific = true
--- prolific.syslog/t/syslog.argv.t.js
+++ prolific.syslog/t/syslog.argv.t.js
@@ -3,7 +3,7 @@ require('proof/redux')(2, require('cadence')(prove))
 function prove (async, assert) {
     var argv = require('../syslog.argv'), program
     async(function () {
-        argv([ '--application', 'a' ], {}, async())
+        argv([ '--application', 'a' ], async())
     }, function (result) {
         assert(result, {
             moduleName: 'prolific.syslog/syslog.processor',
@@ -11,8 +11,5 @@ function prove (async, assert) {
             argv: [],
             terminal: false
         }, 'configuration')
-        program = argv([], { isMainModule: true }, async())
-    }, function () {
-        assert(program.stdout.read() != null, 'inspect')
     })
 }
