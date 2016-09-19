822a2597618a0d900ae48027db9b979cc23572c4
Alan Gutierrez <alan@prettyrobots.com>
2016-09-18 21:43:44 -0500

    Remove interval from Queue.

--- prolific.queue/queue.js
+++ prolific.queue/queue.js
@@ -1,4 +1,5 @@
 var rescue = require('rescue')
+var abend = require('abend')
 var cadence = require('cadence')
 var Chunk = require('prolific.chunk')
 
@@ -8,13 +9,18 @@ function Queue (stream) {
     this._chunkNumber = 1
     this._previousChecksum = 'aaaaaaaa'
     this._stream = stream
-    this._termianted = false
+    this._terminated = false
     this._closed = false
+    this._writing = false
     this._chunks.push(new Chunk(0, new Buffer(''), 1))
 }
 
 Queue.prototype.write = function (buffer) {
     this._buffers.push(buffer)
+    if (!this._writing) {
+        this._writing = true
+        this.flush(abend)
+    }
 }
 
 Queue.prototype._chunkEntries = function () {
@@ -28,54 +34,50 @@ Queue.prototype._chunkEntries = function () {
     this._chunks.push(new Chunk(this._chunkNumber++, buffer, buffer.length))
 }
 
-Queue.prototype._write = cadence(function (async, buffer) {
-    if (this._closed) {
-        throw new Error('bigeasy.prolific.queue#closed')
+Queue.prototype._checkTerminated = function () {
+    if (this._terminated) {
+        throw new Error('bigeasy.prolific.queue#terminated')
     }
+}
+
+Queue.prototype._write = cadence(function (async, buffer) {
     this._stream.write(buffer, async())
 })
 
 Queue.prototype.flush = cadence(function (async) {
     async([function () {
-        if (this._termianted) {
-            return
-        }
-
-        this._chunkEntries()
-
         var loop = async(function () {
+            this._checkTerminated()
             if (this._chunks.length == 0) {
-                return [ loop.break ]
+                this._chunkEntries()
+                if (this._chunks.length == 0) {
+                    this._writing = false
+                    return [ loop.break ]
+                }
             }
             var chunk = this._chunks[0]
             async(function () {
+                this._checkTerminated()
                 this._write(chunk.header(this._previousChecksum), async())
             }, function () {
+                this._checkTerminated()
     // TODO Wait for a response, let's get for reals here.
                 this._write(chunk.buffer, async())
             }, function () {
+                this._checkTerminated()
                 this._previousChecksum = chunk.checksum
                 this._chunks.shift()
             })
         })()
-    }, rescue(/^bigeasy.prolific.queue:closed$/)])
+    }, rescue(/^bigeasy.prolific.queue#terminated$/)])
 })
 
-Queue.prototype.close = function (stderr) {
-    if (!this._closed) {
-        this._closed = true
-        this._stream.end()
-    }
-}
-
 Queue.prototype.exit = function (stderr, callback) {
-    if (this._termianted) {
+    if (this._terminated) {
         return
     }
 
-    this._termianted = true
-
-    this.close()
+    this._terminated = true
 
     this._chunkEntries()
 
--- prolific.queue/t/queue.t.js
+++ prolific.queue/t/queue.t.js
@@ -1,32 +1,47 @@
-require('proof/redux')(3, require('cadence')(prove))
+require('proof/redux')(6, require('cadence')(prove))
 
 function prove (async, assert) {
     var stream = require('stream')
     var Queue = require('../queue')
     var Chunk = require('prolific.chunk')
     var queue
+    var count = 0
+    var expected = [
+        '% 0 aaaaaaaa 811c9dc5 1\n',
+        '',
+        '% 1 811c9dc5 fdaf7437 6\n',
+        '1\n2\n3\n',
+        '% 0 aaaaaaaa 811c9dc5 1\n',
+        ''
+    ]
+    var writable = {
+        write: function (buffer, callback) {
+            assert(buffer.toString(), expected.shift(), 'chunk ' + (++count))
+            this.wait = callback
+        }
+    }
     async(function () {
-        queue = new Queue(new stream.PassThrough)
+        queue = new Queue(writable)
         queue.write(new Buffer(1 + '\n'))
         queue.write(new Buffer(2 + '\n'))
         queue.write(new Buffer(3 + '\n'))
-        queue.flush(async())
     }, function () {
-        var chunk = queue._stream.read().toString()
-        assert(chunk, '% 0 aaaaaaaa 811c9dc5 1\n% 1 811c9dc5 fdaf7437 6\n1\n2\n3\n', 'initialize')
+        while (writable.wait != null) {
+            var wait = [writable.wait, writable.wait = null][0]
+            wait()
+        }
         queue.exit(null)
         queue.exit(null)
-        queue.flush(async())
     }, function () {
-        queue = new Queue(new stream.PassThrough)
+        queue = new Queue(writable)
+        queue.write(new Buffer(1 + '\n'))
         var stderr = new stream.PassThrough
         queue.exit(stderr, function () {})
         var chunk = stderr.read().toString()
-        assert(chunk, '% 0 aaaaaaaa 811c9dc5 0\n% 0 811c9dc5 811c9dc5 1\n', 'exit')
-    }, [function () {
-        queue._write(new Buffer(''), async())
-    }, function (error) {
-        assert(error.message, 'bigeasy.prolific.queue#closed', 'write after close')
-        queue.close()
-    }])
+        assert(chunk, '% 0 aaaaaaaa 811c9dc5 0\n% 0 811c9dc5 811c9dc5 1\n% 1 811c9dc5 05eb07a2 2\n1\n', 'exit')
+        while (writable.wait != null) {
+            var wait = [writable.wait, writable.wait = null][0]
+            wait()
+        }
+    })
 }
