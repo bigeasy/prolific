515ac9f344cc15c77a1fecc74fc342f852169ad6
Alan Gutierrez <alan@prettyrobots.com>
2016-09-19 10:23:03 -0500

    Update example to use latest Shuttle.

--- prolific.example/example.bin.js
+++ prolific.example/example.bin.js
@@ -1,7 +1,7 @@
 var Shuttle = require('prolific.shuttle')
 var logger = require('prolific.logger').createLogger('prolific.example.messages')
 
-Shuttle.shuttle(process, 250, logger)
+Shuttle.shuttle(process, logger)
 
 setInterval(function () {
     logger.info('greeting', { hello: 'world' })
