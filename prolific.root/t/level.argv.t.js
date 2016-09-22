b34996588f0db66de207c9a6bee55c317108b470
Alan Gutierrez <alan@prettyrobots.com>
2016-09-22 09:08:27 -0500

    Fix level argument parser test.

--- prolific.root/t/level.argv.t.js
+++ prolific.root/t/level.argv.t.js
@@ -3,14 +3,16 @@ require('proof/redux')(2, require('cadence')(prove))
 function prove (async, assert) {
     var argv = require('../level'), configuration = { levels: [] }
     async(function () {
-        argv([{ configuration: configuration }, [ 'INFO', 'TRACE=bigeasy.example' ]], {}, async())
+        argv(['INFO', 'TRACE=example' ], {
+            properties: { configuration: configuration }
+        }, async())
     }, function (result) {
         assert(result, { argv: [], terminal: false }, 'result')
         assert(configuration, {
             levels: [{
                 path: '', level: 'INFO'
             }, {
-                path: 'bigeasy.example', level: 'TRACE'
+                path: 'example', level: 'TRACE'
             }]
         }, 'configuration')
     })
