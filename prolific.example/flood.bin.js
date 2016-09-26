b9aaa1ab8945f2ae431d7852b9637058ec02da36
Alan Gutierrez <alan@prettyrobots.com>
2016-09-26 10:15:24 -0500

    Do not run flood quite so long.

--- prolific.example/flood.bin.js
+++ prolific.example/flood.bin.js
@@ -4,7 +4,7 @@ var logger = require('prolific.logger').createLogger('prolific.example.messages'
 var shuttle = Shuttle.shuttle(process, logger)
 
 logger.info('hello')
-for (var i = 0; i < 1000000; i++) {
+for (var i = 0; i < 10000; i++) {
     logger.info('count', { count: i })
 }
 throw new Error
