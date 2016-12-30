c194c0a7b593113cd7f82c51e515c08cf7a1b92b
Alan Gutierrez <alan@prettyrobots.com>
2016-12-29 18:37:04 -0600

    Look in root prolific package for commands.

--- prolific.root/commandable.js
+++ prolific.root/commandable.js
@@ -7,20 +7,24 @@ module.exports = function (terminal, argv) {
         url = command
         command = command.split(':')[0]
     }
-    var pkg = command[0] == '@'
-            ? command.substring(1)
-            : 'prolific.' + command
-    try {
-        var required = require(pkg)
-        if (!required.isProlific) {
-            return null
+    var packages = command[0] == '@'
+                 ? [ command.substring(1) ]
+                 : [ 'prolific.' + command, 'prolific/' + command ]
+    while (packages.length) {
+        var pkg = packages.shift()
+        try {
+            var required = require(pkg)
+            if (!required.isProlific) {
+                continue
+            }
+            argv.shift()
+            if (url != null) {
+                argv.unshift('--url', url)
+            }
+            return required
+        } catch (e) {
+            continue
         }
-        argv.shift()
-        if (url != null) {
-            argv.unshift('--url', url)
-        }
-        return required
-    } catch (e) {
-        return null
     }
+    return null
 }
