b1f7e3a55eeaafbfa96af031eaa102970c70067c
Alan Gutierrez <alan@prettyrobots.com>
2017-05-30 03:52:20 -0500

    Try to pass tests on 0.10.

--- prolific.root/t/prolific.bin.t.js
+++ prolific.root/t/prolific.bin.t.js
@@ -5,12 +5,13 @@ function prove (async, assert) {
     var path = require('path')
 
     var child = path.join(__dirname, 'program.js')
+    var expectedExitCode = /^v0\.10\./.test(process.version) ? 143 : 0
 
     var program
     async(function () {
         program = monitor([ 'configure', 'test', '--key', 'value', 'node', child ], async())
     }, function (code) {
-        assert(code, 0, 'ran')
+        assert(code, expectedExitCode, 'ran')
         var env = JSON.parse(program.stderr.read().toString())
         var configuration = JSON.parse(env.PROLIFIC_CONFIGURATION)
         assert(configuration.configured, 'configured')
