782a43a1938fb93f34a8c55f44f8b824bd2c59c1
Alan Gutierrez <alan@prettyrobots.com>
2017-10-01 13:36:38 -0500

    Add `prolific.resovler` as a dependency.

--- prolific.root/prolific.bin.js
+++ prolific.root/prolific.bin.js
@@ -23,42 +23,58 @@
 
 // Node.js API.
 var assert = require('assert')
-var children = require('child_process')
 var path = require('path')
 var url = require('url')
+var children = require('child_process')
 
+// An evented semaphore.
 var Signal = require('signal')
 
+// Control-flow utilities.
 var cadence = require('cadence')
+var delta = require('delta')
+var or = require('finalist')
+
+// Controlled demolition of objects.
+var Destructible = require('destructible')
+
+// Command line and environment interpretation utilities.
 var inherit = require('prolific.inherit')
 var ipc = require('prolific.ipc')
-var delta = require('delta')
-var exit = require('./exit')
 
-var Thereafter = require('thereafter')
+// Make sense of child exit code.
+var exit = require('./exit')
 
 // Construct a prolific pipeline from a configuration.
 var Pipeline = require('prolific.pipeline')
 
-var Destructible = require('destructible')
+// Monitoring of streams that contain logging messages.
 var Synchronous = require('prolific.consolidator/synchronous')
 var Asynchronous = require('prolific.consolidator/asynchronous')
+
+// TODO Now we require that anyone standing between a root Prolific monitor and
+// a leaf child process use the Descendent library.
 
 // Lots of struggle as I reconsider how to manage errors, return values and the
 // this pointer.
 
+//
 var direct = cadence(function (async, program, inheritance, configuration, argv) {
-    var destructible = new Destructible('prolific')
+    var destructible = new Destructible(5000, 'prolific')
     program.on('shutdown', destructible.destroy.bind(destructible))
-
-    var thereafter = new Thereafter
-    destructible.addDestructor('thereafter', thereafter, 'cancel')
 
     var pipeline = new Pipeline(configuration)
     var synchronous = new Synchronous
     var asynchronous = new Asynchronous(pipeline.processors[0])
 
+    async(function () {
+        destructible.completed.wait(async())
+    }, function (exitCode) {
+        return [ exitCode ]
+    })
+
     async([function () {
+        destructible.destroy()
         pipeline.close(async())
     }], function () {
         pipeline.open(async())
@@ -67,67 +83,57 @@ var direct = cadence(function (async, program, inheritance, configuration, argv)
         ipc(program.ultimate.ipc, process, child)
         destructible.addDestructor('kill', child, 'kill')
         synchronous.addConsumer('0', asynchronous)
-// This is a strange useage where the method is going to return an error, it
-// will be the error used to report.
-        thereafter.run(function (ready) {
-            cadence(function (async) {
-                async(function () {
-                    delta(async()).ee(child).on('exit')
-                    ready.unlatch()
-                }, function (exitCode, signal) {
-                    return exit(exitCode, signal)
-                })
-            })(destructible.monitor('child'))
-        })
-        thereafter.run(function (ready) {
-            cadence(function (async) {
-                synchronous.listen(child.stderr, program.stderr, async())
-                ready.unlatch()
-            })(destructible.monitor('synchronous'))
-        })
-        thereafter.run(function (ready) {
-// It does not seem likely that you'll get an error out of the child pipes and
-// if you do, it is uncommon enough that there is not much more to do for now
-// besides crash. Can't do anything if the synchronous pipe crashes, so maybe
-// only worry about the asynchronous pipe. Ah, so, we could just shutdown when
-// we see the asynchronous pipe error, or maybe even finish for any reason.
-            cadence(function (async) {
-                async([function () {
-                    child.kill()
-                }], function () {
-                    asynchronous.listen(child.stdio[inheritance.fd], async())
-                    ready.unlatch()
-                })
-            })(destructible.monitor('asynchronous'))
-        })
-// TODO Considered giving `Destructible.completed` an array of gathered
-// responses. That way you could use it as the response from the function.
-// Instead I'm calling it here first, except that isn't how Cadence works.
-// Cadence returns the first error caught. Thus, the callback we create above
-// could catch an error, that error would be returned from this function,
-// swallowing a nicer hung interrupt exception.
-//
-// Two directions to go. First, make Cadence return errors in the order
-// specified by their callback creation. Then this can move to the top it is
-// always the first error returns. Second, make destructible gather responeses
-// from monitor and return them in completed. But, the moment I try to work that
-// out in Destructible, it gets confusing. Where to keep them. How to signal you
-// want them gathered. Always gather? What about rescue. Don't gather, right?
-//
-// If you do use some sort of gathered completed, then you can't really make too
-// much use of the returns. Also, you'll note that all of the above will
-// rethrow their errors, so this timeout is not actually going to cause a hang
-// to timeout.
-//
-// Hello, again. Considering how it worked before. There was this orthoginal
-// timer that would raise an uncatchable exception and crash the program because
-// the inability to quit is more of a programmer error than a run time error.
-// Here I'm trying to funnel that exception to the completed latch. When using
-// the Cadence-friendly Destructible invocation this will mean that we're adding
-// a wait for many sub-Cadences.
-        destructible.completed(5000, async())
+
+        cadence(function (async) {
+            async(function () {
+                delta(async()).ee(child).on('close')
+            }, function (exitCode, signal) {
+                return exit(exitCode, signal)
+            })
+        })(destructible.monitor('child'))
+
+        // No additional destructor. Will close when standard error closes.
+        synchronous.listen(child.stderr, program.stderr, destructible.monitor('synchronous'))
+        // It does not seem likely that you'll get an error out of the child
+        // pipes and if you do, it is uncommon enough that there is not much
+        // more to do for now besides crash. Can't do anything if the
+        // synchronous pipe crashes, so maybe only worry about the asynchronous
+        // pipe. Ah, so, we could just shutdown when we see the asynchronous
+        // pipe error, or maybe even finish for any reason.
+        asynchronous.listen(child.stdio[inheritance.fd], destructible.monitor('asynchronous'))
+
+        // TODO Considered giving `Destructible.completed` an array of gathered
+        // responses. That way you could use it as the response from the
+        // function. Instead I'm calling it here first, except that isn't how
+        // Cadence works. Cadence returns the first error caught. Thus, the
+        // callback we create above could catch an error, that error would be
+        // returned from this function, swallowing a nicer hung interrupt
+        // exception.
+        //
+        // Two directions to go. First, make Cadence return errors in the order
+        // specified by their callback creation. Then this can move to the top
+        // it is always the first error returns. Second, make destructible
+        // gather responeses from monitor and return them in completed. But, the
+        // moment I try to work that out in Destructible, it gets confusing.
+        // Where to keep them. How to signal you want them gathered. Always
+        // gather? What about rescue. Don't gather, right?
+        //
+        // If you do use some sort of gathered completed, then you can't really
+        // make too much use of the returns. Also, you'll note that all of the
+        // above will rethrow their errors, so this timeout is not actually
+        // going to cause a hang to timeout.
+        //
+        // Hello, again. Considering how it worked before. There was this
+        // orthoginal timer that would raise an uncatchable exception and crash
+        // the program because the inability to quit is more of a programmer
+        // error than a run time error. Here I'm trying to funnel that exception
+        // to the completed latch. When using the Cadence-friendly Destructible
+        // invocation this will mean that we're adding a wait for many
+        // sub-Cadences.
+        destructible.completed.wait(async())
     }, function () {
         asynchronous.exit()
+        return []
     })
 })
 
