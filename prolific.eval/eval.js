b9dc05004d2763b7ecf98dc3d9579e6a4893cd7e
Alan Gutierrez <alan@prettyrobots.com>
2017-10-18 12:30:47 -0500

    Reimplement as function builder.

--- prolific.eval/eval.js
+++ prolific.eval/eval.js
@@ -1,14 +1,17 @@
 exports.create = function () {
     var vargs = Array.prototype.slice.call(arguments)
     var source = vargs.pop()
-    return Function.apply(Function, [
+    var f = Function.apply(Function, [
         '$', '$qualifier', '$level', 'ERROR', 'WARN', 'INFO', 'DEBUG', 'TRACE'
     ].concat(vargs, 'return ' + source))
-}
-
-exports.invoke = function () {
-    var vargs = Array.prototype.slice.call(arguments)
-    var f = vargs.shift()
-    var entry = vargs.shift()
-    return f.apply(null, [ entry.json, entry.qualifier, entry.level, 4, 3, 1, 0, 0 ].concat(vargs))
+    return function () {
+        var vargs = Array.prototype.slice.call(arguments)
+        var entry = vargs.shift()
+        return f.apply(null, [
+            entry.json,
+            entry.qualifier,
+            entry.level,
+            4, 3, 1, 0, 0
+        ].concat(vargs))
+    }
 }
--- prolific.eval/t/eval.t.js
+++ prolific.eval/t/eval.t.js
@@ -4,6 +4,5 @@ function prove (okay) {
     var eval = require('..')
     var f = eval.create('$x', '$.value + $x + INFO')
     var entry = { json: { value: 1 } }
-
-    okay(eval.invoke(f, entry, 3), 5, 'eval')
+    okay(f(entry, 3), 5, 'eval')
 }
