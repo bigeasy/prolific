40b5304e808fdc39db40e632e04d305ae26cb4b2
Alan Gutierrez <alan@prettyrobots.com>
2017-10-18 12:44:09 -0500

    Rename to Prolific Evaluator.

--- prolific.eval/t/evaluator.t.js
+++ prolific.eval/t/evaluator.t.js
@@ -1,8 +1,8 @@
 require('proof')(1, prove)
 
 function prove (okay) {
-    var eval = require('..')
-    var f = eval.create('$x', '$.value + $x + INFO')
+    var evaluator = require('..')
+    var f = evaluator.create('$x', '$.value + $x + INFO')
     var entry = { json: { value: 1 } }
     okay(f(entry, 3), 5, 'eval')
 }
