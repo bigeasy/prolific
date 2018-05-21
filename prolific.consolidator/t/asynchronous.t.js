0641ea8a9c467a0aab7867f59f8a97b0bc846f06
Alan Gutierrez <alan@prettyrobots.com>
2018-05-21 11:22:37 -0500

    Fix tests for Node.js 10.

--- prolific.consolidator/t/asynchronous.t.js
+++ prolific.consolidator/t/asynchronous.t.js
@@ -1,6 +1,6 @@
-require('proof')(2, prove)
+require('proof')(2, require('cadence')(prove))
 
-function prove (assert) {
+function prove (async, okay) {
     var stream = require('stream')
     var Asynchronous = require('../asynchronous')
     var Chunk = require('prolific.chunk')
@@ -17,69 +17,72 @@ function prove (assert) {
     })
     asynchronous.listen(through, abend)
 
-    var json = new Buffer(JSON.stringify({
+    var json = Buffer.from(JSON.stringify({
         qualifier: 'prolific.consolidator',
         level: 'warn',
         when: 0
     }) + '\n')
 
-    chunk = new Chunk(1, 0, new Buffer(''), 1)
-    write(through, chunk, 'aaaaaaaa')
+    chunk = new Chunk(1, 0, Buffer.from(''), 1)
+    async(function () {
+        write(through, chunk, 'aaaaaaaa', async())
+    }, function () {
+        previousChecksum = chunk.checksum
+        buffer = Buffer.from('{}\n')
+        chunk = new Chunk(1, 1, json, json.length)
+        write(through, chunk, previousChecksum, async())
+    }, function () {
 
-    previousChecksum = chunk.checksum
-    buffer = new Buffer('{}\n')
-    chunk = new Chunk(1, 1, json, json.length)
-    write(through, chunk, previousChecksum)
+        okay(entries.shift(), {
+            level: 3,
+            when: 0,
+            qualifier: [ null, 'prolific', 'prolific.consolidator' ],
+            formatted: [],
+            json: { when: 0, level: 'warn', qualifier: 'prolific.consolidator' }
+        }, 'read chunk')
 
-    assert(entries.shift(), {
-        level: 3,
-        when: 0,
-        qualifier: [ null, 'prolific', 'prolific.consolidator' ],
-        formatted: [],
-        json: { when: 0, level: 'warn', qualifier: 'prolific.consolidator' }
-    }, 'read chunk')
+        asynchronous.consume({
+            pid: chunk.pid,
+            number: chunk.number,
+            previousChecksum: previousChecksum,
+            checksum: chunk.checksum,
+            buffer: chunk.buffer,
+            value: chunk.value
+        })
 
-    asynchronous.consume({
-        pid: chunk.pid,
-        number: chunk.number,
-        previousChecksum: previousChecksum,
-        checksum: chunk.checksum,
-        buffer: chunk.buffer,
-        value: chunk.value
+        previousChecksum = chunk.checksum
+
+        json = Buffer.from(JSON.stringify({
+            qualifier: 'prolific.consolidator',
+            level: 'warn',
+            when: 0
+        }))
+        chunk = new Chunk(1, 2, json, json.length)
+
+        asynchronous.consume({
+            pid: chunk.pid,
+            number: chunk.number,
+            previousChecksum: previousChecksum,
+            checksum: chunk.checksum,
+            buffer: chunk.buffer.toString('utf8'),
+            value: chunk.value
+        })
+
+        asynchronous.consume({ eos: true })
+
+        asynchronous.exit()
+
+        okay(entries.shift(), {
+            level: 3,
+            when: 0,
+            qualifier: [ null, 'prolific', 'prolific.consolidator' ],
+            formatted: [],
+            json: { when: 0, level: 'warn', qualifier: 'prolific.consolidator' }
+        }, 'read chunk from sync')
     })
 
-    previousChecksum = chunk.checksum
-
-    json = new Buffer(JSON.stringify({
-        qualifier: 'prolific.consolidator',
-        level: 'warn',
-        when: 0
-    }))
-    chunk = new Chunk(1, 2, json, json.length)
-
-    asynchronous.consume({
-        pid: chunk.pid,
-        number: chunk.number,
-        previousChecksum: previousChecksum,
-        checksum: chunk.checksum,
-        buffer: chunk.buffer.toString('utf8'),
-        value: chunk.value
-    })
-
-    asynchronous.consume({ eos: true })
-
-    asynchronous.exit()
-
-    assert(entries.shift(), {
-        level: 3,
-        when: 0,
-        qualifier: [ null, 'prolific', 'prolific.consolidator' ],
-        formatted: [],
-        json: { when: 0, level: 'warn', qualifier: 'prolific.consolidator' }
-    }, 'read chunk from sync')
-
-    function write (writable, chunk, previousChecksum) {
+    function write (writable, chunk, previousChecksum, callback) {
         writable.write(chunk.header(previousChecksum))
-        writable.write(chunk.buffer)
+        writable.write(chunk.buffer, callback)
     }
 }
--- prolific.consolidator/t/synchronous.t.js
+++ prolific.consolidator/t/synchronous.t.js
@@ -1,6 +1,6 @@
-require('proof')(4, prove)
+require('proof')(4, require('cadence')(prove))
 
-function prove (okay, callback) {
+function prove (async, okay) {
     var stream = require('stream')
     var Synchronous = require('../synchronous')
     var Chunk = require('prolific.chunk')
@@ -12,58 +12,62 @@ function prove (okay, callback) {
     var forward = new stream.PassThrough
     var synchronous = new Synchronous
 
+    var done = async()
     synchronous.listen(through, forward, function (error) {
         okay(! error, 'listen exit okay')
-        callback()
+        done()
     })
 
     through.write('hello, world\n')
 
-    chunk = new Chunk(0, 0, new Buffer(''), 1)
+    chunk = new Chunk(0, 0, Buffer.from(''), 1)
     write(through, chunk, 'aaaaaaaa')
 
     previousChecksum = chunk.checksum
-    buffer = new Buffer('a\n')
+    buffer = Buffer.from('a\n')
     chunk = new Chunk(0, 1, buffer, buffer.length)
     write(through, chunk, previousChecksum)
 
-    chunk = new Chunk(1, 0, new Buffer(''), 1)
+    chunk = new Chunk(1, 0, Buffer.from(''), 1)
     write(through, chunk, 'aaaaaaaa')
 
     previousChecksum = chunk.checksum
-    buffer = new Buffer('a\n')
+    buffer = Buffer.from('a\n')
     chunk = new Chunk(1, 1, buffer, buffer.length)
-    write(through, chunk, previousChecksum)
+    var consumer
+    async(function () {
+        write(through, chunk, previousChecksum, async())
+    }, function () {
+        okay(forward.read().toString(), 'hello, world\n', 'through')
 
-    okay(forward.read().toString(), 'hello, world\n', 'through')
-
-    var consumer = {
-        chunks: [],
-        consume: function (chunk) {
-            this.chunks.push(chunk)
+        consumer = {
+            chunks: [],
+            consume: function (chunk) {
+                this.chunks.push(chunk)
+            }
         }
-    }
 
-    synchronous.addConsumer(1, consumer)
+        synchronous.addConsumer(1, consumer)
 
-    okay(consumer.chunks.shift().buffer.toString(), 'a\n', 'consumer join')
+        okay(consumer.chunks.shift().buffer.toString(), 'a\n', 'consumer join')
 
-    previousChecksum = chunk.checksum
-    buffer = new Buffer('b\n')
-    chunk = new Chunk(1, 2, buffer, buffer.length)
-    write(through, chunk, previousChecksum)
+        previousChecksum = chunk.checksum
+        buffer = Buffer.from('b\n')
+        chunk = new Chunk(1, 2, buffer, buffer.length)
+        write(through, chunk, previousChecksum, async())
+    }, function () {
+        okay(consumer.chunks.shift().buffer.toString(), 'b\n', 'consumer consume')
 
-    okay(consumer.chunks.shift().buffer.toString(), 'b\n', 'consumer consume')
+        previousChecksum = chunk.checksum
+        chunk = new Chunk(1, 3, Buffer.from(''), 0)
+        chunk.checksum = 'aaaaaaaa'
+        write(through, chunk, previousChecksum, async())
+    }, function () {
+        through.end()
+    })
 
-    previousChecksum = chunk.checksum
-    chunk = new Chunk(1, 3, new Buffer(''), 0)
-    chunk.checksum = 'aaaaaaaa'
-    write(through, chunk, previousChecksum)
-
-    through.end()
-
-    function write (writable, chunk, previousChecksum) {
+    function write (writable, chunk, previousChecksum, callback) {
         writable.write(chunk.header(previousChecksum))
-        writable.write(chunk.buffer)
+        writable.write(chunk.buffer, callback)
     }
 }
