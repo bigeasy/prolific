c419d99091e990cd2b3367f26866f93578c1e220
Alan Gutierrez <alan@prettyrobots.com>
2018-07-22 14:09:20 -0500

    Working example of reloading.

--- prolific.example/example.bin.js
+++ prolific.example/example.bin.js
@@ -3,12 +3,13 @@ var logger = require('prolific.logger').createLogger('prolific.example')
 
 var shuttle = Shuttle.shuttle(process, logger)
 
-process.once('SIGTERM', function () {
+process.on('SIGTERM', function () {
     shuttle.close()
 })
 
 setInterval(function () {
     console.log('sending')
+    console.log(process.memoryUsage())
     logger.info('greeting', { hello: 'world' })
     logger.info('metric', { metric: true, measure: 1 })
     logger.error('error', { value: 1 })
