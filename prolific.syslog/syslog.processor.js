49735b2762f9891b24f86dc4abffce7324cfa782
Alan Gutierrez <alan@prettyrobots.com>
2018-07-25 23:18:41 -0500

    Use Prolific Facility module.

--- prolific.syslog/syslog.processor.js
+++ prolific.syslog/syslog.processor.js
@@ -1,31 +1,7 @@
 var url = require('url')
 var coalesce = require('extant')
 
-var FACILITY = {
-    kern: 0,
-    user: 1,
-    mail: 2,
-    daemon: 3,
-    auth: 4,
-    syslog: 5,
-    lpr: 6,
-    news: 7,
-    uucp: 8,
-    clock: 9,
-    sec: 10,
-    ftp: 11,
-    ntp: 12,
-    audit: 13,
-    alert: 14,
-    local0: 16,
-    local1: 17,
-    local2: 18,
-    local3: 19,
-    local4: 20,
-    local5: 21,
-    local6: 22,
-    local7: 23
-}
+var FACILITY = require('prolific.facility')
 
 var LEVEL = {
     trace: 7,
