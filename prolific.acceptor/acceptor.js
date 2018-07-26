e43c9507ef58f6ffecf5adcf85c0741b3bfaac1f
Alan Gutierrez <alan@prettyrobots.com>
2018-07-26 00:47:25 -0500

    Use Prolific Level module.

--- prolific.acceptor/acceptor.js
+++ prolific.acceptor/acceptor.js
@@ -1,17 +1,4 @@
-var LEVEL = {
-    panic: 0,
-    emerg: 0,
-    alert: 1,
-    crit: 2,
-    err: 3,
-    error: 3,
-    warning: 4,
-    warn: 4,
-    notice: 5,
-    info: 6,
-    debug: 7
-}
-
+var LEVEL = require('prolific.level')
 var Evaluator = require('prolific.evaluator')
 
 function Acceptor (accept, chain) {
