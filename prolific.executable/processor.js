24d9d221fe0ee2908d04213c781b34900ce064d1
Alan Gutierrez <alan@prettyrobots.com>
2018-07-28 04:06:09 -0500

    Debug a production exception.

--- prolific.executable/processor.js
+++ prolific.executable/processor.js
@@ -100,6 +100,7 @@ Processor.prototype.process = cadence(function (async, envelope) {
     var entries = lines.map(JSON.parse)
     var loop = async(function () {
         while (entries.length && !Array.isArray(entries[0])) {
+            console.log(entries)
             this._processor.process(entries.shift())
         }
         if (entries.length == 0) {
