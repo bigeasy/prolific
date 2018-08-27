ecc8f34b2b679b96861942b0ebc978f04754b3ae
Alan Gutierrez <alan@prettyrobots.com>
2018-08-27 02:49:52 -0500

    Rename `pid` to `id`.

--- prolific.chunk/chunk.js
+++ prolific.chunk/chunk.js
@@ -1,8 +1,8 @@
 var fnv = require('hash.fnv')
 var hex = require('./hex')
 
-function Chunk (pid, number, buffer, value) {
-    this.pid = pid
+function Chunk (id, number, buffer, value) {
+    this.id = id
     this.number = number
     this.checksum = hex(fnv(0, buffer, 0, buffer.length), 8)
     this.value = value
@@ -12,7 +12,7 @@ function Chunk (pid, number, buffer, value) {
 Chunk.prototype.header = function (previousChecksum, length) {
     return new Buffer([
         '% ',
-        this.pid, ' ',
+        this.id, ' ',
         this.number, ' ',
         previousChecksum, ' ',
         this.checksum, ' ',
