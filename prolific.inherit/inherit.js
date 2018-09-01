a84a96f33d3c181d39de7eba4c9cf8e39c32c6eb
Alan Gutierrez <alan@prettyrobots.com>
2018-09-01 05:46:41 -0500

    Let caller decide to override `2` with `pipe`.

--- prolific.inherit/inherit.js
+++ prolific.inherit/inherit.js
@@ -23,7 +23,5 @@ module.exports = function (program) {
         stdio.push('ipc')
     }
 
-    stdio[2] = 'pipe'
-
     return stdio
 }
--- prolific.inherit/t/inherit.t.js
+++ prolific.inherit/t/inherit.t.js
@@ -8,11 +8,11 @@ function prove (assert) {
         env: { INHERIT_FD: 5 },
         arrayed: { inherit: [ 0, 1, 2, 4, 4, 'INHERIT_FD', 8 ] }
     })
-    assert(inheritance, [ 0, 1, 'pipe', 'ignore', 4, 5, 'ignore', 'ignore', 8 ], 'inherit')
+    assert(inheritance, [ 0, 1, 2, 'ignore', 4, 5, 'ignore', 'ignore', 8 ], 'inherit')
 
     inheritance = inherit({
         ultimate: { ipc: true },
         arrayed: { inherit: [ 0, 1, 2, 4, 4, 5 ] }
     })
-    assert(inheritance, [ 0, 1, 'pipe', 'ignore', 4, 5, 'ipc' ], 'inherit with ipc')
+    assert(inheritance, [ 0, 1, 2, 'ignore', 4, 5, 'ipc' ], 'inherit with ipc')
 }
