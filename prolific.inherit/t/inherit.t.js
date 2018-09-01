4460fc852c3ba401dbb61c39a044b8f70897c32e
Alan Gutierrez <alan@prettyrobots.com>
2018-09-01 06:55:46 -0500

    Rename `assert` to `okay`.

--- prolific.inherit/t/inherit.t.js
+++ prolific.inherit/t/inherit.t.js
@@ -1,11 +1,11 @@
 require('proof')(1, prove)
 
-function prove (assert) {
+function prove (okay) {
     var inherit = require('../inherit'), inheritance
 
     inheritance = inherit({
         env: { INHERIT_FD: 5 },
         arrayed: { inherit: [ 0, 1, 2, 4, 4, 'INHERIT_FD', 8 ] }
     })
-    assert(inheritance, [ 0, 1, 2, 'ignore', 4, 5, 'ignore', 'ignore', 8 ], 'inherit')
+    okay(inheritance, [ 0, 1, 2, 'ignore', 4, 5, 'ignore', 'ignore', 8 ], 'inherit')
 }
