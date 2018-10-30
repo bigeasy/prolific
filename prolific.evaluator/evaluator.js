91264112c1d5c829c0dfcfb2349874a3b598c4c4
Alan Gutierrez <alan@prettyrobots.com>
2018-10-30 07:37:43 -0500

    Expect source to be a string.

--- prolific.evaluator/evaluator.js
+++ prolific.evaluator/evaluator.js
@@ -1,6 +1,6 @@
-exports.create = function (f, require) {
+exports.create = function (source, require) {
     try {
-        return new Function('require', 'return ' + f.toString())(require)()
+        return new Function('require', 'return ' + source)(require)()
     } catch (error) {
         return null
     }
--- prolific.evaluator/t/evaluator.t.js
+++ prolific.evaluator/t/evaluator.t.js
@@ -3,7 +3,7 @@ require('proof')(2, prove)
 function prove (okay) {
     var Evaluator = require('..')
     var modularized = require('./modularized')
-    var triage = Evaluator.create(modularized.triage, require)
+    var triage = Evaluator.create(modularized.triage.toString(), require)
     okay(triage(0, { label: 'acceptable' }), true, 'created')
     okay(Evaluator.create(modularized.broken, require), null, 'failed')
 }
