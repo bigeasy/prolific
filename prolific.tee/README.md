b04dc0ee5d60880dd45e36b0f4da0578758a89b1
Alan Gutierrez <alan@prettyrobots.com>
2018-11-01 00:29:01 -0500

    Deprecate Prolific Tee.

--- prolific.tee/README.md
+++ prolific.tee/README.md
@@ -1,1 +1,5 @@
 ## Prolific Tee
+
+Depreciated. No longer using quite so many bits of middleware.
+
+See [Prolific](http://github.com/bigeasy/prolific).
--- prolific.tee/t/tee.processor.t.js
+++ prolific.tee/t/tee.processor.t.js
@@ -1,35 +1,9 @@
-require('proof')(2, require('cadence')(prove))
+require('proof')(1, prove)
 
-function prove (async, okay) {
-    var Tee = require('..')
-
-    var processed = []
-    var nextProcessor = {
-        process: function (entry) {
-            processed.push(entry)
-        }
+function prove (okay) {
+    try {
+        require('..')
+    } catch (error) {
+        okay(error.message, 'deprecated', 'deprecated')
     }
-
-    var Destructible = require('destructible')
-    var destructible = new Destructible('t/tee.processor.t')
-
-    var entry = { level: 'info', json: { a: 1, qualifier: 'module' } }
-    async([function () {
-        destructible.destroy()
-    }], function () {
-        destructible.monitor('tee', Tee, { pipeline: [] }, nextProcessor, async())
-    }, function (processor) {
-        processor.process(entry)
-        okay(processed.shift(), entry, 'processed')
-        destructible.monitor('tee', Tee, {
-            accept: false,
-            chain: [{ accept: true, path: '.other' }],
-            pipeline: [],
-            consume: true
-        }, nextProcessor, async())
-    }, function (processor) {
-        processor.process({ level: 7, json: { a: 1, qualifier: 'other' } })
-        processor.process(entry)
-        okay(processed, [ entry ], 'consumed')
-    })
 }
