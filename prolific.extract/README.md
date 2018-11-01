4ead3bfba840d3a39975f65f2d613b42823deeaa
Alan Gutierrez <alan@prettyrobots.com>
2018-11-01 01:15:23 -0500

    Deprecate Prolific Extract.

--- prolific.extract/README.md
+++ prolific.extract/README.md
@@ -1,3 +1,5 @@
-## Prolific Filter
+## Prolific Extract
 
-Filter Prolific logging messages.
+Depreciated. No longer using quite so many bits of middleware.
+
+See [Prolific](http://github.com/bigeasy/prolific).
--- prolific.extract/t/extract.processor.t.js
+++ prolific.extract/t/extract.processor.t.js
@@ -1,93 +1,9 @@
-require('proof')(2, prove)
+require('proof')(1, prove)
 
-function prove (okay, callback) {
-    var Processor = require('../extract.processor')
-    var Destructible = require('destructible')
-    var destructible = new Destructible('t/extract.processor.t')
-
-    destructible.completed.wait(callback)
-
-    var cadence = require('cadence')
-
-    var processed = []
-    var sink = {
-        process: function (entry) { processed.push(entry) }
+function prove (okay) {
+    try {
+        require('..')
+    } catch (error) {
+        okay(error.message, 'deprecated', 'deprecated')
     }
-
-    cadence(function (async) {
-        async(function () {
-            destructible.monitor('processor', Processor, {
-                consume: true,
-                extract: {
-                    'specific#message': '{ key: $.value }'
-                }
-            }, sink, async())
-        }, function (processor) {
-            processor.process({
-                formatted: [],
-                level: 0,
-                env: {},
-                json: {
-                    when: 0,
-                    pid: 0,
-                    value: 1
-                }
-            })
-            okay(processed.splice(0), [{
-                level: 5,
-                formatted: [],
-                env: {},
-                json: {
-                    key: 1,
-                    qualified: 'specific#message',
-                    qualifier: 'specific',
-                    label: 'message',
-                    level: 'notice',
-                    when: 0,
-                    pid: 0
-                }
-            }], 'consume')
-        }, function () {
-            destructible.monitor('processor', Processor, {
-                qualifier: 'example',
-                extract: {
-                    message: '{ key: $.value }'
-                }
-            }, sink, async())
-        }, function (processor) {
-            processor.process({
-                formatted: [],
-                level: 0,
-                env: {},
-                json: {
-                    when: 0,
-                    pid: 0,
-                    value: 1
-                }
-            })
-            okay(processed.splice(0), [{
-                level: 5,
-                formatted: [],
-                env: {},
-                json: {
-                    key: 1,
-                    qualified: 'example#message',
-                    qualifier: 'example',
-                    label: 'message',
-                    level: 'notice',
-                    when: 0,
-                    pid: 0
-                }
-            }, {
-                formatted: [],
-                level: 0,
-                env: {},
-                json: {
-                    when: 0,
-                    pid: 0,
-                    value: 1
-                }
-            }], 'inject')
-        })
-    })(destructible.monitor('test'))
 }
