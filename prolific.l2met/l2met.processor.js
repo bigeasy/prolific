29be663ed723d959e563dc2dbd808ab2ca787a84
Alan Gutierrez <alan@prettyrobots.com>
2018-11-01 13:22:36 -0500

    Convert l2met to a simple formatter.

--- prolific.l2met/l2met.processor.js
+++ prolific.l2met/l2met.processor.js
@@ -1,27 +1,19 @@
 var coalesce = require('extant')
 
-function Processor (nextProcessor) {
-    this._nextProcessor = nextProcessor
-}
-
-Processor.prototype.process = function (entry) {
-    if (/^(count|measure|sample)$/.test(entry.json.l2met)) {
-        var json = entry.json
-        var formatted = [ json.l2met.substring(0, 1), '#', json.label, '=', json.value, coalesce(json.unit, '') ]
-        var separator = ' tags='
-        for (var tag in json.tags || {}) {
-            var value = json.tags[tag]
-            formatted.push(separator, tag, ':', value)
-            separator = ','
+module.exports = function (type, label, value, unit, tags) {
+    if (typeof unit != 'string') {
+        if (unit != null) {
+            tags = unit
+            unit = ''
         }
-        formatted.push('\n')
-        entry.formatted.push(formatted.join(''))
     }
-    this._nextProcessor.process(entry)
+    var formatted = [ type.substring(0, 1), '#', label, '=', value, coalesce(unit, '') ]
+    var separator = ' tags='
+    for (var tag in tags || {}) {
+        var value = tags[tag]
+        formatted.push(separator, tag, ':', value)
+        separator = ','
+    }
+    formatted.push('\n')
+    return formatted.join('')
 }
-
-module.exports = function (destructible, configuration, nextProcessor, callback) {
-    callback(null, new Processor(nextProcessor))
-}
-
-module.exports.isProlificProcessor = true
--- prolific.l2met/t/l2met.processor.t.js
+++ prolific.l2met/t/l2met.processor.t.js
@@ -1,42 +1,12 @@
-require('proof')(2, prove)
+require('proof')(3, prove)
 
-function prove (okay, callback) {
-    var Processor = require('..')
+function prove (okay) {
+    var L2met = require('..')
 
-    var Destructible = require('destructible')
-    var destructible = new Destructible
-
-    var cadence = require('cadence')
-
-    destructible.completed.wait(callback)
-
-    var sink = {
-        gathered: [],
-        process: function (entry) {
-            this.gathered.push(entry)
-        }
-    }
-
-    destructible.monitor('test', cadence(function (async) {
-        async([function () {
-            destructible.destroy()
-        }], function () {
-            destructible.monitor('Processor', Processor, {}, sink, async())
-        }, function (processor) {
-            processor.process({
-                json: { label: 'count', l2met: 'count', value: 1, tags: { key: 'value' } },
-                formatted: []
-            })
-            processor.process({
-                json: { label: 'count', l2met: 'count', value: 1 },
-                formatted: []
-            })
-            processor.process({
-                json: { l2met: 'steve' },
-                formatted: []
-            })
-            okay(sink.gathered.shift().formatted[0], 'c#count=1 tags=key:value\n', 'format')
-            okay(sink.gathered.shift().formatted[0], 'c#count=1\n', 'format to tags')
-        })
-    }), null)
+    var l2met = L2met('count', 'count', 1, { key: 'value' })
+    okay(l2met, 'c#count=1 tags=key:value\n', 'format no unit')
+    var l2met = L2met('count', 'count', 1, 'ms')
+    okay(l2met, 'c#count=1ms\n', 'format no tags')
+    var l2met = L2met('count', 'count', 1)
+    okay(l2met, 'c#count=1\n', 'format no tags or unit')
 }
