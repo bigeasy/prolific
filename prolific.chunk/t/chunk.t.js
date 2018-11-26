3e01c4e54f17c5eaa3d45cde33b0f9cde8d1fa39
Alan Gutierrez <alan@prettyrobots.com>
2018-11-25 21:23:03 -0600

    Rename `assert` to `okay`.

--- prolific.chunk/t/chunk.t.js
+++ prolific.chunk/t/chunk.t.js
@@ -1,17 +1,17 @@
 require('proof')(8, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Chunk = require('..')
     var chunk = new Chunk(1, 0, new Buffer(''), 1)
-    assert(chunk.id, 1, 'id')
-    assert(chunk.number, 0, 'initial number')
-    assert(chunk.checksum, '811c9dc5', 'initial checksum')
-    assert(chunk.header('aaaaaaaa').toString(), '% 1 0 aaaaaaaa 811c9dc5 1\n', 'initial header')
+    okay(chunk.id, 1, 'id')
+    okay(chunk.number, 0, 'initial number')
+    okay(chunk.checksum, '811c9dc5', 'initial checksum')
+    okay(chunk.header('aaaaaaaa').toString(), '% 1 0 aaaaaaaa 811c9dc5 1\n', 'initial header')
     var checksum = chunk.checksum
     var buffer = Buffer('a\n')
     var chunk = new Chunk(1, 1, buffer, buffer.length)
-    assert(chunk.id, 1, 'subsequent id')
-    assert(chunk.number, 1, 'subsequent number')
-    assert(chunk.checksum, '2524c6d2', 'subsequent checksum')
-    assert(chunk.header(checksum).toString(), '% 1 1 811c9dc5 2524c6d2 2\n', 'subsequent header')
+    okay(chunk.id, 1, 'subsequent id')
+    okay(chunk.number, 1, 'subsequent number')
+    okay(chunk.checksum, '2524c6d2', 'subsequent checksum')
+    okay(chunk.header(checksum).toString(), '% 1 1 811c9dc5 2524c6d2 2\n', 'subsequent header')
 }
--- prolific.chunk/t/hex.t.js
+++ prolific.chunk/t/hex.t.js
@@ -1,7 +1,7 @@
 require('proof')(2, prove)
 
-function prove (assert) {
+function prove (okay) {
     var hex = require('../hex')
-    assert('00000000', hex(0, 8), 'padding')
-    assert('ffffffff', hex(0xffffffff, 8), 'padding')
+    okay('00000000', hex(0, 8), 'padding')
+    okay('ffffffff', hex(0xffffffff, 8), 'padding')
 }
