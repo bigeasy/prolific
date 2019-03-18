4f94b66ebc01b6f966e4ba7c7cc3a762d9150777
Alan Gutierrez <alan@prettyrobots.com>
2019-03-18 00:44:16 -0500

    Mostly fixed with Chunk headers.
    Because we want to fit within `PIPE_BUF` we want to have a simplified
    header with a fixed width so that it is easy to split a buffer knowing
    an exact size for each segment. We no longer length encode the buffer,
    it must always end with a newline. We require that the payload is always
    JSON, so we know that they body will never have newlines itself. We no
    longer keep track of a series number in the header since its character
    width is variable, we rely on the linked list of the checksums.
    
    There was a value in the header that was used to encode length for
    ordinary bodies and for headers it might indicate the previous series
    number when continuing a closed monitor socket from standard error. We'd
    detect a new stream with a zero series number and assert that the
    previous checksum was a constant value. We can indicate a header by
    detecting an id that has not yet been used instead.
    
    We do have one variable entry which is a chunk count. For our happy path
    socket it will always be `1`. When it comes time to split things up for
    writing on standard error, the first entry of a split entry will be the
    count of chunks including the first chunk. For subsequent chunks the
    value is always a single digit, `1` for for all chunks except the final
    chunk which will be `0`.
    
    Notes on `PIPE_BUF`. https://serverfault.com/a/733611

--- prolific.chunk/chunk.js
+++ prolific.chunk/chunk.js
@@ -1,23 +1,49 @@
+// A fast hash.
 var fnv = require('hash.fnv')
+
+// Format a hexadecimal number with leading zeros.
 var hex = require('./hex')
 
-function Chunk (id, number, buffer, value) {
+// Note that we assume the buffer has a newline appended and our checksum does
+// not include the newline.
+
+//
+function Chunk (id, count, buffer) {
     this.id = id
-    this.number = number
-    this.checksum = hex(fnv(0, buffer, 0, buffer.length), 8)
-    this.value = value
+    this.count = count
+    this.checksum = hex(fnv(0, buffer, 0, buffer.length - 1), 8)
     this.buffer = buffer
 }
 
+// Because we want to fit within `PIPE_BUF` we want to have a simplified header
+// with a fixed width so that it is easy to split a buffer knowing an exact size
+// for each segment. We no longer length encode the buffer, it must always end
+// with a newline. We require that the payload is always JSON, so we know that
+// they body will never have newlines itself. We no longer keep track of a
+// series number in the header since its character width is variable, we rely on
+// the linked list of the checksums.
+//
+// There was a value in the header that was used to encode length for ordinary
+// bodies and for headers it might indicate the previous series number when
+// continuing a closed monitor socket from standard error. We'd detect a new
+// stream with a zero series number and assert that the previous checksum was a
+// constant value. We can indicate a header by detecting an id that has not yet
+// been used instead.
+//
+// We do have one variable entry which is a chunk count. For our happy path
+// socket it will always be `1`. When it comes time to split things up for
+// writing on standard error, the first entry of a split entry will be the count
+// of chunks including the first chunk. For subsequent chunks the value is
+// always a single digit, `1` for for all chunks except the final chunk which
+// will be `0`.
+//
+// Notes on `PIPE_BUF`. https://serverfault.com/a/733611
+
+//
 Chunk.prototype.header = function (previousChecksum, length) {
-    return Buffer.from([
-        '\n% ',
-        this.id, ' ',
-        this.number, ' ',
-        previousChecksum, ' ',
-        this.checksum, ' ',
-        this.value, '\n'
-    ].join(''))
+    return Buffer.from('\n% ' + this.id + ' ' + previousChecksum +
+        ' ' + this.checksum + ' ' + this.count + '\n')
 }
 
+// Export the object.
 module.exports = Chunk
--- prolific.chunk/test/chunk.t.js
+++ prolific.chunk/test/chunk.t.js
@@ -2,16 +2,16 @@ require('proof')(8, prove)
 
 function prove (okay) {
     var Chunk = require('..')
-    var chunk = new Chunk(1, 0, Buffer.from(''), 1)
+    var chunk = new Chunk(1, 1, Buffer.from('\n'))
     okay(chunk.id, 1, 'id')
-    okay(chunk.number, 0, 'initial number')
     okay(chunk.checksum, '811c9dc5', 'initial checksum')
-    okay(chunk.header('aaaaaaaa').toString(), '\n% 1 0 aaaaaaaa 811c9dc5 1\n', 'initial header')
+    okay(chunk.count, 1, 'count')
+    okay(chunk.header('aaaaaaaa').toString(), '\n% 1 aaaaaaaa 811c9dc5 1\n', 'initial header')
     var checksum = chunk.checksum
     var buffer = Buffer.from('a\n')
-    var chunk = new Chunk(1, 1, buffer, buffer.length)
+    var chunk = new Chunk(1, 1, buffer)
     okay(chunk.id, 1, 'subsequent id')
-    okay(chunk.number, 1, 'subsequent number')
-    okay(chunk.checksum, '2524c6d2', 'subsequent checksum')
-    okay(chunk.header(checksum).toString(), '\n% 1 1 811c9dc5 2524c6d2 2\n', 'subsequent header')
+    okay(chunk.checksum, 'e40c292c', 'subsequent checksum')
+    okay(chunk.count, 1, 'subsequent count')
+    okay(chunk.header(checksum).toString(), '\n% 1 811c9dc5 e40c292c 1\n', 'subsequent header')
 }
