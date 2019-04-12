b13f4b0d52d3ea246c4a22328b09f3e2f4fde0c9
Alan Gutierrez <alan@prettyrobots.com>
2019-04-11 21:44:46 -0500

    Rename package.
    Not going to create an additional InfluxDB modules, do not need to
    qualify. Transport is via TCP or an HTTP POST, so I created a generic
    Prolific HTTP module.

--- prolific.influxdb/test/influxdb.t.js
+++ prolific.influxdb/test/influxdb.t.js
@@ -1,42 +1,42 @@
 require('proof')(7, prove)
 
 function prove (okay) {
-    var format = require('..')
-    okay(format({
+    var influxdb = require('..')
+    okay(influxdb({
         measurement: 'a',
         fields: { field: 1 }
     }), 'a field=1\n', 'measurement only')
-    okay(format({
+    okay(influxdb({
         measurement: 'a',
         fields: { field: 1 },
         newline: false
     }), 'a field=1', 'no newline')
-    okay(format({
+    okay(influxdb({
         measurement: 'a',
         fields: { field$i: 1 }
     }), 'a field=1i\n', 'integer field')
-    okay(format({
+    okay(influxdb({
         measurement: 'a',
         fields: { field: 1 },
         timestamp: 0
     }), 'a field=1 0\n', 'zero timestamp')
-    okay(format({
+    okay(influxdb({
         measurement: 'a',
         fields: { field: 1 },
         timestamp: -1
     }), 'a field=1 -1000000\n', 'non-zero timestamp')
-    okay(format({
+    okay(influxdb({
         measurement: 'a',
         fields: { field: 1 },
         tags: { tag: 'value' }
     }), 'a,tag=value field=1\n', 'with tags')
-    console.log(format({
+    console.log(influxdb({
         measurement: 'a',
         fields: { field: '\'must quote\'' },
         tags: { tag: 'comma,space equals=' },
         newline: false
     }))
-    okay(format({
+    okay(influxdb({
         measurement: 'a',
         fields: { field: '\'must quote\'' },
         tags: { tag: 'comma,space equals=' }
