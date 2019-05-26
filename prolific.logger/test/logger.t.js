7d483cdb932779c9d6728e352b1a3dd42a4762ed
Alan Gutierrez <alan@prettyrobots.com>
2019-05-25 23:37:21 -0500

    Convert tests to Mocha.

--- prolific.logger/test/logger.t.js
+++ prolific.logger/test/logger.t.js
@@ -1,52 +1,53 @@
-require('proof')(5, prove)
-
-function prove (okay) {
-    var Logger = require('..')
-    var slice = [].slice
-
-    var tests = [ 'concat', 'log', 'named' ]
-    var prolific = require('prolific.resolver')
-    prolific.sink.json = function () {
-        var vargs = slice.call(arguments)
-        okay(vargs, [
-            'error',
-            'hello',
-            'greeting',
-            { a: 1 },
-            { pid: process.pid }
-        ], tests.shift())
-    }
-    var logger = Logger.createLogger('hello')
-    logger.concat('error', 'greeting', {}, { a: 1 })
-    logger.log('error', 'greeting', { a: 1 })
-    logger.error('greeting', { a: 1 })
-    var expect = [{
-        vargs: [
-            'panic',
-            'hello',
-            'exception',
-            { code: 'EOUCH', key: 'value', stack: false },
-            { pid: process.pid }
-        ],
-        message: 'level, context, properties'
-    }, {
-        vargs: [
-            'error',
-            'hello',
-            'exception',
-            { stack: true, code: 'EOUCH' },
-            { pid: process.pid }
-        ],
-        message: 'no level nor context nor properties'
-    }]
-    prolific.sink.json = function () {
-        var vargs = slice.call(arguments)
-        var expected = expect.shift()
-        vargs[3].stack = !! vargs[3].stack
-        okay(vargs, expected.vargs, expected.message)
-    }
-    var error = new Error('ouch')
-    error.code = 'EOUCH'
-    logger.stackTrace('panic', 'exception', { key: 'value' }, [ 'code', 'missing' ])(error)
-    logger.stackTrace('exception')(error)
-}
+describe('logger', () => {
+    const assert = require('assert')
+    const Logger = require('..')
+    it('can log', () => {
+        const test = []
+        const prolific = require('prolific.resolver')
+        prolific.sink.json = function (...vargs) {
+            test.push(vargs)
+        }
+        const logger = Logger.createLogger('hello')
+        logger.concat('error', 'greeting', {}, { a: 1 })
+        logger.log('error', 'greeting', { a: 1 })
+        logger.error('greeting', { a: 1 })
+        var expect = [{
+            vargs: [
+                'panic',
+                'hello',
+                'exception',
+                { code: 'EOUCH', key: 'value', stack: false },
+                { pid: process.pid }
+            ],
+            message: 'level, context, properties'
+        }, {
+            vargs: [
+                'error',
+                'hello',
+                'exception',
+                { stack: true, code: 'EOUCH' },
+                { pid: process.pid }
+            ],
+            message: 'no level nor context nor properties'
+        }]
+        prolific.sink.json = function (...vargs) {
+            test.push(vargs)
+        }
+        const error = new Error('ouch')
+        error.code = 'EOUCH'
+        logger.stackTrace('panic', 'exception', { key: 'value' }, [ 'code', 'missing' ])(error)
+        logger.stackTrace('exception')(error)
+        test[test.length - 1][3].stack = !! test[test.length - 1][3].stack
+        assert.deepStrictEqual(test, [[
+            'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
+        ], [
+            'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
+        ], [
+            'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
+        ], [
+            'panic', 'hello', 'exception', { code: 'EOUCH', key: 'value' }, { pid: process.pid }
+        ], [
+            'error', 'hello', 'exception', { code: 'EOUCH', stack: true }, { pid: process.pid }
+        ]], 'test')
+    })
+})
