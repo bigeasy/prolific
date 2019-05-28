a0c4ac353f9ecdc41c3b6cc0dec0afc62c2a2300
Alan Gutierrez <alan@prettyrobots.com>
2019-05-28 00:37:51 -0500

    Convert Prolific Consolidator for ES6.

--- prolific.consolidator/consolidator.js
+++ prolific.consolidator/consolidator.js
@@ -1,67 +1,62 @@
-var cadence = require('cadence')
-var byline = require('byline')
-var Staccato = require('staccato')
+const byline = require('byline')
+const Staccato = { Readable: require('staccato/readable') }
+const events = require('events')
+const Prolific = { Error: require('prolific.error') }
 
-function Consolidator (asynchronous, synchronous, queue) {
-    this._exited = false
-    this._series = 1
-    this._readable = {
-        asynchronous: new Staccato.Readable(byline(asynchronous)),
-        synchronous: new Staccato.Readable(byline(synchronous))
+class Consolidator extends events.EventEmitter {
+    constructor (asynchronous, synchronous, queue) {
+        super()
+        this._exited = false
+        this._series = 1
+        this._streams = { asynchronous: byline(asynchronous), synchronous: byline(synchronous) }
+        asynchronous.on('error', this._error.bind(this, 'asynchronous'))
+        synchronous.on('error', this._error.bind(this, 'synchronous'))
+        this._readable = {
+            asynchronous: new Staccato.Readable(this._streams.asynchronous),
+            synchronous: new Staccato.Readable(this._streams.synchronous)
+        }
+        this._queue = queue
     }
-    this._queue = queue
-}
 
-Consolidator.prototype.asynchronous = cadence(function (async) {
-    async.loop([], function () {
-        async(function () {
-            this._readable.asynchronous.read(async())
-        }, function  (line) {
-            if (line == null) {
-                this._readable.asynchronous.raise()
-                return [ async.break ]
-            }
+    _error (stream, error) {
+        this.emit('error', new Prolific.Error('read', [ error ], { stream }))
+    }
+
+    async asynchronous () {
+        for await (const line of this._readable.asynchronous) {
             this._queue.push(JSON.parse(line.toString()))
             this._series++
-        })
-    })
-})
-
-Consolidator.prototype.synchronous = cadence(function (async) {
-    async.loop([], function () {
-        async(function () {
-            this._readable.synchronous.read(async())
-        }, function  (line) {
-            if (line == null) {
-                this._readable.asynchronous.raise()
-                this.exit()
-                return [ async.break ]
-            }
-            this._synchronous(JSON.parse(line.toString()))
-        })
-    })
-})
-
-Consolidator.prototype._synchronous = function (chunk) {
-    this._readable.asynchronous.destroy()
-    switch (chunk.method) {
-    case 'entries':
-        if (chunk.series == this._series) {
-            this._series++
-            this._queue.push(chunk.entries)
         }
-        break
-    case 'exit':
-        this._exited = true
-        this._readable.synchronous.destroy()
-        this._queue.push([[{ method: 'exit' }]])
-        break
     }
-}
 
-Consolidator.prototype.exit = function () {
-    if (!this._exited) {
-        this._synchronous({ method: 'exit' })
+    async synchronous () {
+        for await (const line of this._readable.synchronous) {
+            this._synchronous(JSON.parse(line.toString()))
+        }
+        this.exit()
+    }
+
+    _synchronous (chunk) {
+        this._streams.asynchronous.destroy()
+        switch (chunk.method) {
+        case 'entries':
+            if (chunk.series == this._series) {
+                this._series++
+                this._queue.push(chunk.entries)
+            }
+            break
+        case 'exit':
+            this._exited = true
+            this._streams.synchronous.destroy()
+            this._queue.push([[{ method: 'exit' }]])
+            break
+        }
+    }
+
+    exit () {
+        if (!this._exited) {
+            this._synchronous({ method: 'exit' })
+        }
     }
 }
 
--- prolific.consolidator/test/consolidator.t.js
+++ prolific.consolidator/test/consolidator.t.js
@@ -1,47 +1,43 @@
-require('proof')(3, prove)
-
-function prove (okay, callback) {
-    var Destructible = require('destructible')
-
-    var destructible = new Destructible('test/consolidator')
-
-    destructible.completed.wait(callback)
-
-    var cadence = require('cadence')
-
-    var stream = require('stream')
-
-    var Consolidator = require('../consolidator')
-
-    var synchronous = new stream.PassThrough
-    var asynchronous = new stream.PassThrough
-
-    var queue = []
-    var consolidator = new Consolidator(asynchronous, synchronous, queue)
-
-    consolidator.asynchronous(destructible.ephemeral('asynchronous'))
-    consolidator.synchronous(destructible.ephemeral('synchronous'))
-
-    cadence(function (async) {
-        async(function () {
-            asynchronous.write(JSON.stringify([{ value: 1 }]) + '\n', async())
-        }, function () {
-            okay(queue.shift(), [{ value: 1 }], 'async')
+describe('consolidator', () => {
+    const assert = require('assert')
+    const stream = require('stream')
+    const Consolidator = require('../consolidator')
+    it('can consolidate', async () => {
+        const synchronous = new stream.PassThrough
+        const asynchronous = new stream.PassThrough
+        const queue = []
+        const consolidator = new Consolidator(asynchronous, synchronous, queue)
+        const async = consolidator.asynchronous()
+        const sync = consolidator.synchronous()
+        asynchronous.write(JSON.stringify([{ value: 1 }]) + '\n')
+        await new Promise(resolve => setImmediate(resolve))
+        assert.deepStrictEqual(queue.shift(), [{ value: 1 }], 'async')
             synchronous.write(JSON.stringify({
                 method: 'entries',
                 series: 1,
                 entries: [{ value: 2 }]
-            }) + '\n', async())
-        }, function () {
-            okay(queue.length, 0, 'earlier in series')
-            synchronous.write(JSON.stringify({
-                method: 'entries',
-                series: 2,
-                entries: [{ value: 2 }]
-            }) + '\n', async())
-        }, function () {
-            okay(queue.shift(), [{ value: 2 }], 'sync')
-            consolidator.exit()
-        })
-    })(destructible.durable('test'))
-}
+            }) + '\n')
+        await new Promise(resolve => setImmediate(resolve))
+        assert.equal(queue.length, 0, 'earlier in series')
+        synchronous.write(JSON.stringify({
+            method: 'entries',
+            series: 2,
+            entries: [{ value: 2 }]
+        }) + '\n')
+        await new Promise(resolve => setImmediate(resolve))
+        assert.deepStrictEqual(queue.shift(), [{ value: 2 }], 'sync')
+        consolidator.exit()
+        await async
+        await sync
+    })
+    it('can report a stream error', () => {
+        const test = []
+        const synchronous = new stream.PassThrough
+        const asynchronous = new stream.PassThrough
+        const queue = []
+        const consolidator = new Consolidator(asynchronous, synchronous, queue)
+        consolidator.on('error', (error) => test.push(error.stream))
+        synchronous.emit('error', new Error('error'))
+        assert.deepStrictEqual(test, [ 'synchronous' ], 'test')
+    })
+})
