fb61ae622538bdb0f64fa2e71a1d49c5d1d51878
Alan Gutierrez <alan@prettyrobots.com>
2019-08-28 22:22:58 -0500

    Convert Syslog to ES6 class.
    Closes #993.

--- prolific.syslog/syslog.js
+++ prolific.syslog/syslog.js
@@ -1,33 +1,35 @@
-var coalesce = require('extant')
+const coalesce = require('extant')
 
-var FACILITY = require('prolific.facility')
-var LEVEL = require('prolific.level')
+const FACILITY = require('prolific.facility')
+const LEVEL = require('prolific.level')
 
-function Processor (configuration) {
-    this._application = coalesce(configuration.application, process.title)
-    this._hostname = coalesce(configuration.hostname, 'localhost')
-    this._facility = FACILITY[coalesce(configuration.facility, 'local0')]
-    this._serializer = coalesce(configuration.serializer, JSON)
-}
+class Processor {
+    constructor (configuration) {
+        this._application = coalesce(configuration.application, process.title)
+        this._hostname = coalesce(configuration.hostname, 'localhost')
+        this._facility = FACILITY[coalesce(configuration.facility, 'local0')]
+        this._serializer = coalesce(configuration.serializer, JSON)
+    }
 
-Processor.prototype.format = function (entry) {
-    var pid = entry.pid, when = entry.when
-    // TODO Probably faster if I set `undefined`.
-    delete entry.when
-    delete entry.pid
-    var line = [
-        '<' + (this._facility * 8 + LEVEL[entry.level]) + '>1',
-        new Date(when).toISOString(),
-        this._hostname,
-        this._application,
-        coalesce(pid, '-'),
-        '-',
-        '-',
-        this._serializer.stringify(entry)
-    ]
-    entry.when = when
-    entry.pid = pid
-    return line.join(' ') + '\n'
+    format (entry) {
+        const pid = entry.pid, when = entry.when
+        // TODO Probably faster if I set `undefined`.
+        delete entry.when
+        delete entry.pid
+        const line = [
+            '<' + (this._facility * 8 + LEVEL[entry.level]) + '>1',
+            new Date(when).toISOString(),
+            this._hostname,
+            this._application,
+            coalesce(pid, '-'),
+            '-',
+            '-',
+            this._serializer.stringify(entry)
+        ]
+        entry.when = when
+        entry.pid = pid
+        return line.join(' ') + '\n'
+    }
 }
 
 module.exports = Processor
