224c57c67cd9759e71f4b7721b5dae4cdfb707aa
Alan Gutierrez <alan@prettyrobots.com>
2019-09-09 14:57:52 -0500

    Implement temporary directory generator.
    Closes #1012.

---
+++ prolific.executable/tmp.js
@@ -0,0 +1,18 @@
+const fs = require('fs').promises
+const path = require('path')
+const assert = require('assert')
+
+module.exports = async function (tmp, random, pid) {
+    for (;;) {
+        const umask = process.umask(0o077)
+        try {
+            const dirname = path.join(tmp, `prolific-${pid}-${await random()}`)
+            await fs.mkdir(dirname)
+            return dirname
+        } catch (error) {
+            assert(error.code == 'EEXIST')
+        } finally {
+            process.umask(umask)
+        }
+    }
+}
