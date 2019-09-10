2f3171a1611039d96c180d1c39d1cada264b7386
Alan Gutierrez <alan@prettyrobots.com>
2019-09-10 01:45:47 -0500

    Move gathering utility into Prolific Collector.
    See #1020.

--- prolific.collector/collector.js
+++ prolific.collector/collector.js
@@ -1,164 +1,49 @@
-const fnv = require('hash.fnv')
 const assert = require('assert')
 
-class Collector {
-    constructor (stderr, outbox) {
-        this._channels = {}
-        this._stderr = stderr
-        this.outbox = outbox
+const ascension = require('ascension')
+const events = require('events')
+
+const sort = ascension([ Number, Number ], entry => [ entry.when, entry.series ])
+
+class Collector extends events.EventEmitter {
+    constructor () {
+        super()
+        this._streams = []
     }
 
-    _chunk (matched, buffer) {
-        if (matched.header) {
-            const json = JSON.parse(buffer.toString())
-            switch (json.method) {
-            case 'announce':
-                assert(this._channels[json.id] == null)
-                this._channels[matched.id] = {
-                    chunk: null,
-                    checksum: matched.checksum
+    exit (pid) {
+        for (const path in this._streams) {
+            const stream = this._streams[path]
+            if (stream.path[stream.path.length - 1] === pid) {
+                if (stream.queue.length) {
+                    this.emit('notice', {
+                        level: 'warn', label: 'gap',
+                        start: stream.start, path: stream.path
+                    })
                 }
-                this.outbox.push({
-                    method: 'announce',
-                    id: matched.id,
-                    body: json.body
-                })
-                break
-            case 'entries':
-                assert(this._channels[matched.id] != null)
-                this._channels[matched.id].chunk = {
-                    checksum: json.checksum,
-                    series: json.series,
-                    chunks: json.chunks,
-                    index: 0,
-                    buffers: []
-                }
-                this._channels[matched.id].checksum = matched.checksum
-                break
-            case 'exit':
-                assert(this._channels[matched.id] != null)
-                assert(this._channels[matched.id].chunk == null)
-                delete this._channels[matched.id]
-                this.outbox.push({ method: 'exit', id: matched.id })
+                delete this._streams[path]
                 break
             }
-        } else {
-            const chunk = this._channels[matched.id].chunk
-            chunk.buffers.push(buffer)
-            chunk.index++
-            if (chunk.index == chunk.chunks) {
-                this._channels[matched.id].chunk = null
-                const buffer = Buffer.concat(chunk.buffers)
-                const checksum = fnv(0, buffer, 0, buffer.length)
-                assert(checksum == chunk.checksum)
-                this.outbox.push({
-                    method: 'entries',
-                    id: matched.id,
-                    series: chunk.series,
-                    entries: JSON.parse(buffer.toString())
-                })
-            }
-            this._channels[matched.id].checksum = matched.checksum
         }
     }
 
-    // Because we only write chunks that are `PIPE_BUF` or smaller, we know that
-    // they will always be complete in the standard output stream. We cannot
-    // rely on them being on their own line however. They maybe be interpolated
-    // into then non-chunk standard output spew of another child process so that
-    // the headers do not start on a new line, rather they start in the middle
-    // of someone else's line. When this happens, we do know that they will be
-    // at the end of the line because they will be written entirely and thier
-    // newline character will break the line they've been dropped into. Thus, we
-    // can search for our headers at the end of each line in standard output
-    // where, for the most part,  the end of the line starts at the very
-    // beginning of the line.
-    //
-    // We write the stuff before the header to standard error. Again, this is
-    // going be an empty string more often than not.
-
-    //
-    _scan (buffer, offset) {
-        let i = offset
-        for (let I = buffer.length; i < I; i++) {
-            if (buffer[i] == 0xa) {
-                if (this._remainder != null) {
-                    this._readLine(Buffer.concat([ this._remainder, buffer.slice(offset, i) ]), true)
-                    this._remainder = null
-                } else {
-                    this._readLine(buffer.slice(offset, i), true)
-                }
-                return i + 1
+    data (data) {
+        const path = data.path.join('/')
+        if (this._streams[path] == null) {
+            this._streams[path] = {
+                start: data.start,
+                path: data.path,
+                series: 0,
+                queue: []
             }
         }
-        this._remainder = Buffer.from(buffer.slice(offset, buffer.length))
-        return i
-    }
-
-    scan (buffer) {
-        let offset = 0
-        while (offset != buffer.length) {
-            offset = this._scan(buffer, offset)
-        }
-    }
-
-    _writeStandardError (line, newline) {
-        this._stderr.write(line)
-        if (newline) {
-            this._stderr.write('\n')
-        }
-    }
-
-    _readLine (line, newline) {
-        if (this.matched == null) {
-            line = line.toString()
-            const $ = /^(.*)% (\d+\/\d+) ([0-9a-f]{8}) ([0-9a-f]{8}) ([10]) %$/i.exec(line)
-            if ($) {
-                this.matched = {
-                    line: line,
-                    preceding: $[1],
-                    id: $[2],
-                    checksum: $[3],
-                    previous: $[4],
-                    header: $[5] == '1'
-                }
-                if (
-                    (
-                        this._channels[this.matched.id] != null &&
-                        this._channels[this.matched.id].checksum == this.matched.previous
-                    )
-                    ||
-                    this.matched.previous == 'aaaaaaaa'
-                ) {
-                    return
-                }
-            }
-            this.matched = null
-            this._writeStandardError(line, newline)
-        } else {
-            const checksum = fnv(0, line, 0, line.length)
-            if (checksum == parseInt(this.matched.checksum, 16)) {
-                this._writeStandardError(this.matched.preceding, false)
-                this._chunk(this.matched, line)
-            } else {
-                this._writeStandardError(this.matched.line, true)
-                this._writeStandardError(line, newline)
-            }
-            this.matched = null
-        }
-    }
-
-    // Absence of a newline means that any match is not a valid chunk.
-
-    //
-    end () {
-        if (this.matched != null) {
-            this._writeStandardError(this.matched.line, true)
-            this.matched = null
-        }
-        if (this._remainder != null) {
-            this._writeStandardError(this._remainder, false)
-            this._remainder = null
+        const stream = this._streams[path]
+        assert.equal(stream.start, data.start, 'start time mismatch')
+        stream.queue.push(data)
+        stream.queue.sort(sort)
+        while (stream.queue.length != 0 && stream.series == stream.queue[0].series) {
+            stream.series = (stream.series + 1) & 0xffffff
+            this.emit('data', stream.queue.shift())
         }
     }
 }
--- prolific.collector/test/collector.test.js
+++ prolific.collector/test/collector.test.js
@@ -1,88 +1,48 @@
 describe('collector', () => {
     const assert = require('assert')
-    const stream = require('stream')
-    const chunks = [
-        '% 1/2 224e8640 aaaaaaaa 1 %',
-        '{"method":"announce","body":1}',
-         '% 1/2 a8d518e8 224e8640 1 %',
-        '{"method":"entries","series":1,"checksum":3417670023,"chunks":2}',
-        '% 1/2 a6a72ac5 a8d518e8 0 %',
-        '[{"alphabet":["abcdefghijklmnopqrstuvwxyz","abcdefghijklmnopqrstuvwxyz"',
-        '% 1/2 c1e07487 a6a72ac5 0 %',
-        ',"abcdefghijklmnopqrstuvwxyz"]}]',
-        '% 1/2 b798da34 c1e07487 1 %',
-        '{"method":"exit"}',
-        ''
-    ]
-    const Collector = require('../collector')
-    it('can collect chunks', () => {
-        const output = new stream.PassThrough
-
-        const collector = new Collector(output, [])
-
-        collector.scan(Buffer.from('err'))
-        collector.scan(Buffer.from('or\n'))
-        assert.equal(output.read().toString(), 'error\n', 'pass through')
-
-        collector.scan(Buffer.from('% 1/2 00000000 00000000 1 %\n'))
-        assert.deepStrictEqual(output.read().toString().split('\n'), [
-            '% 1/2 00000000 00000000 1 %',
-            ''
-        ], 'header with bad start')
-
-        collector.scan(Buffer.from(`abc${chunks[0]}\n${chunks[1]}\ndef\n`))
-        assert.equal(output.read().toString(), 'abcdef\n', 'pass through interpolated')
-        assert.deepStrictEqual(collector.outbox.shift(), {
-            method: 'announce',
-            id: '1/2',
-            body: 1
-        }, 'announce')
-
-        collector.scan(Buffer.from('% 1/2 00000000 00000000 1 %\n'))
-        assert.deepStrictEqual(output.read().toString().split('\n'), [
-            '% 1/2 00000000 00000000 1 %',
-            ''
-        ], 'header with bad previous checksum')
-
-        collector.scan(Buffer.from(chunks[2] + '\nabcdef\n'))
-        assert.deepStrictEqual(output.read().toString().split('\n'), [
-            chunks[2],
-            'abcdef',
-            ''
-        ], 'bad body checksum')
-
-        collector.scan(Buffer.from(chunks.slice(2).join('\n')))
-
-        assert.deepStrictEqual(collector.outbox.shift(), {
-            method: 'entries',
+    const Collector = require('..')
+    it('can order synchronous messages', () => {
+        const collector = new Collector
+        const test = []
+        collector.on('data', data => test.push(data.name))
+        collector.on('notice', message => test.push(message.label))
+        collector.data({
+            start: 0,
             series: 1,
-            id: '1/2',
-            entries: [{
-                alphabet: [
-                    'abcdefghijklmnopqrstuvwxyz',
-                    'abcdefghijklmnopqrstuvwxyz',
-                    'abcdefghijklmnopqrstuvwxyz'
-                ]
-            }]
-        }, 'entries')
-
-        assert.deepStrictEqual(collector.outbox.shift(), {
-            method: 'exit',
-            id: '1/2',
-        }, 'exit')
-
-        collector.end()
-        assert.equal(output.read(), null, 'end of line at end of stream')
+            when: 0,
+            path: [ 1, 2 ],
+            name: 'exit'
+        })
+        collector.data({
+            start: 0,
+            series: 0,
+            when: 0,
+            path: [ 1, 2 ],
+            name: 'start'
+        })
+        collector.exit(2)
+        assert.deepStrictEqual(test, [ 'start', 'exit' ], 'ordered')
     })
-    it('can terminate partial matches at stream end', () => {
-        const output = new stream.PassThrough
-        const collector = new Collector(output)
-        collector.scan(Buffer.from(chunks[0] + '\n'))
-        collector.scan(Buffer.from(chunks[1].substring(0, 3)))
-        collector.end()
-        assert.deepStrictEqual(output.read().toString().split('\n'), [
-            chunks[0],
-            '{"m'
-        ], 'end during match with no newline at end of file')
+    it('can exit with missing messages', () => {
+        const collector = new Collector
+        const test = []
+        collector.on('data', data => test.push(data.name))
+        collector.on('notice', message => test.push(message.label))
+        collector.data({
+            start: 0,
+            series: 0,
+            when: 0,
+            path: [ 1, 2 ],
+            name: 'start'
+        })
+        collector.data({
+            start: 0,
+            series: 2,
+            when: 0,
+            path: [ 1, 2 ],
+            name: 'exit'
+        })
+        collector.exit(2)
+        assert.deepStrictEqual(test, [ 'start', 'gap' ], 'ordered')
     })
 })
