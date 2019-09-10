bf7c03d146a6d3635dc3839491306309d32a108f
Alan Gutierrez <alan@prettyrobots.com>
2019-09-09 19:27:35 -0500

    Create a directory watcher.
    Create a directory watcher that will pluck, read, parse and remove a
    file containing a single JSON object.
    
    Closes #1013.

---
+++ prolific.executable/test/watcher.test.js
@@ -0,0 +1,95 @@
+describe('watcher', () => {
+    const assert = require('assert')
+    const rimraf = require('rimraf')
+    const path = require('path')
+    const fs = require('fs').promises
+    const callback = require('prospective/callback')
+    const once = require('prospective/once')
+    const sink = require('foremost')('prolific.sink')
+    const events = require('events')
+    const entries = new events.EventEmitter
+    entries.log = []
+    const cache = {}
+    before(() => {
+        cache.json = sink.json
+        sink.json = (...vargs) => {
+            entries.log.push(vargs)
+            entries.emit('entry')
+        }
+    })
+    after(() => {
+        sink.json = cache.json
+    })
+    const TMPDIR = path.join(__dirname, 'tmp')
+    const dir = {
+        stage: path.resolve(TMPDIR, 'stage'),
+        publish: path.resolve(TMPDIR, 'publish')
+    }
+    process.on('unhandledRejection', error => { throw error })
+    async function reset () {
+        await callback(callback => rimraf(TMPDIR, callback))
+        await fs.mkdir(dir.publish, { recursive: true })
+        await fs.mkdir(dir.stage, { recursive: true })
+        // For some reason we need to wait a bit for the above directories to
+        // actually take effect on OS X, otherwise files from previous run are
+        // extant and the first events are reporting a missing file.
+        await new Promise(resolve => setTimeout(resolve, 50))
+        const Watcher = require('../watcher')
+        const Destructible = require('destructible')
+        const destructible = new Destructible(__filename)
+        const watcher = new Watcher(destructible, () => 0, path.join(TMPDIR, 'publish'))
+        return { destructible, watcher }
+    }
+    it('can watch and parse a directory', async () => {
+        const { watcher, destructible } = await reset()
+        const data = once(watcher, 'data')
+        const entry = once(entries, 'entry')
+        await fs.writeFile(path.join(dir.stage, 'stage.jsons'), '1')
+        await fs.rename(path.join(dir.stage, 'stage.jsons'),
+                        path.join(dir.publish, 'publish-00000000.jsons'))
+        const [ json ] = await data
+        assert.equal(json, 1, 'received')
+        await entry
+        destructible.destroy()
+        await destructible.promise
+        const log = entries.log.splice(0)
+        assert.equal(log[0][2], 'read', 'read')
+        assert.equal(log[1][2], 'unlink', 'unlink')
+    })
+    it('can detect a bad file name', async () => {
+        const { watcher, destructible } = await reset()
+        const entry = once(entries, 'entry')
+        await fs.writeFile(path.join(dir.stage, 'stage.jsons'), '1')
+        await fs.rename(path.join(dir.stage, 'stage.jsons'),
+                        path.join(dir.publish, 'publish.jsons'))
+        await entry
+        destructible.destroy()
+        await destructible.promise
+        const log = entries.log.splice(0)
+        assert.equal(log[0][2], 'filename', 'filename error')
+    })
+    it('can detect a bad checksum', async () => {
+        const { watcher, destructible } = await reset()
+        const entry = once(entries, 'entry')
+        await fs.writeFile(path.join(dir.stage, 'stage.jsons'), '1')
+        await fs.rename(path.join(dir.stage, 'stage.jsons'),
+                        path.join(dir.publish, 'publish-1.jsons'))
+        await entry
+        destructible.destroy()
+        await destructible.promise
+        const log = entries.log.splice(0)
+        assert.equal(log[0][2], 'checksum', 'checksum error')
+    })
+    it('can detect bad json', async () => {
+        const { watcher, destructible } = await reset()
+        const entry = once(entries, 'entry')
+        await fs.writeFile(path.join(dir.stage, 'stage.jsons'), '{')
+        await fs.rename(path.join(dir.stage, 'stage.jsons'),
+                        path.join(dir.publish, 'publish-0.jsons'))
+        await entry
+        destructible.destroy()
+        await destructible.promise
+        const log = entries.log.splice(0)
+        assert.equal(log[0][2], 'json', 'json error')
+    })
+})
---
+++ prolific.executable/watcher.js
@@ -0,0 +1,77 @@
+const filesystem = require('fs')
+const fs = require('fs').promises
+const events = require('events')
+const Queue = require('avenue')
+const path = require('path')
+const assert = require('assert')
+const logger = require('prolific.logger').createLogger('prolific')
+const coalesce = require('extant')
+
+const LEVEL = { ENOENT: 'debug' }
+
+class Watcher extends events.EventEmitter {
+    constructor (destructible, hash, directory) {
+        super()
+        this._queue = new Queue
+        this._hash = hash
+        this._directory = directory
+        this._watcher = filesystem.watch(directory)
+        this._watcher.on('change', (eventType, filename) => {
+            this._queue.push({ eventType, filename })
+        })
+        this._watcher.on('error', this.emit.bind(this, 'error'))
+        destructible.durable('change', this._change(this._queue.shifter()))
+        destructible.destruct(() => this._watcher.close())
+        destructible.destruct(() => this._queue.push(null))
+    }
+
+    async _change (shifter) {
+        for await (const { eventType, filename } of shifter.iterator()) {
+            const hash = /-[0-9af]{1,8}\.jsons$/.exec(filename)
+            if (hash == null) {
+                logger.warn('filename', { directory: this._directory, eventType, filename })
+                continue
+            }
+            let buffer = null
+            try {
+                buffer = await fs.readFile(path.resolve(this._directory, filename))
+            } catch (error) {
+                const level = coalesce(LEVEL[error.code], 'warn')
+                logger[level]('read', {
+                    directory: this._directory,
+                    eventType, filename, message: error.message, code: error.code
+                })
+            }
+            try {
+                buffer = await fs.readFile(path.resolve(this._directory, filename))
+                await fs.unlink(path.resolve(this._directory, filename))
+            } catch (error) {
+                const level = coalesce(LEVEL[error.code], 'warn')
+                logger[level]('unlink', {
+                    directory: this._directory,
+                    eventType, filename, message: error.message, code: error.code
+                })
+                continue
+            }
+            const expected = parseInt(hash, 16)
+            const actual = this._hash.call(null, buffer)
+            if (actual != expected) {
+                logger.warn('checksum', {
+                    directory: this._directory, eventType, filename, actual, expected
+                })
+            }
+            const json = buffer.toString()
+            try {
+                this.emit('data', JSON.parse(json))
+            } catch (error) {
+                assert(error instanceof SyntaxError, 'syntax error expected')
+                logger.warn('json', {
+                    directory: this._directory,
+                    eventType, filename, json
+                })
+            }
+        }
+    }
+}
+
+module.exports = Watcher
