16adad75d1b5740fd3674b064b272985944f3fe2
Alan Gutierrez <alan@prettyrobots.com>
2019-09-12 22:56:44 -0500

    Use `pid` to track streams instead of `path`.
    Moving away from using `Descendent` to pass the socket around and will
    be using the `pid` as the one true identifier for a user process.
    
    Closes #1060.

--- prolific.collector/collector.js
+++ prolific.collector/collector.js
@@ -11,39 +11,33 @@ class Collector extends events.EventEmitter {
         this._streams = []
     }
 
-    exit (pid) {
-        for (const path in this._streams) {
-            const stream = this._streams[path]
-            if (stream.path[stream.path.length - 1] === pid) {
-                if (stream.queue.length) {
-                    this.emit('notice', {
-                        level: 'warn', label: 'gap',
-                        start: stream.start, path: stream.path
-                    })
-                }
-                delete this._streams[path]
-                break
-            }
-        }
-    }
-
     data (data) {
-        const path = data.path.join('/')
-        if (this._streams[path] == null) {
-            this._streams[path] = {
+        if (this._streams[data.pid] == null) {
+            this._streams[data.pid] = {
                 start: data.start,
-                path: data.path,
+                pid: data.pid,
                 series: 0,
                 queue: []
             }
         }
-        const stream = this._streams[path]
-        assert.equal(stream.start, data.start, 'start time mismatch')
-        stream.queue.push(data)
-        stream.queue.sort(sort)
-        while (stream.queue.length != 0 && stream.series == stream.queue[0].series) {
-            stream.series = (stream.series + 1) & 0xffffff
-            this.emit('data', stream.queue.shift())
+        const stream = this._streams[data.pid]
+        if (data.body.method == 'eos') {
+            if (stream.queue.length) {
+                this.emit('notice', {
+                    level: 'warn', label: 'gap',
+                    start: stream.start, path: stream.path
+                })
+            }
+            this.emit('data', data)
+            delete this._streams[data.pid]
+        } else {
+            assert.equal(stream.start, data.start, 'start time mismatch')
+            stream.queue.push(data)
+            stream.queue.sort(sort)
+            while (stream.queue.length != 0 && stream.series == stream.queue[0].series) {
+                stream.series = (stream.series + 1) & 0xffffff
+                this.emit('data', stream.queue.shift())
+            }
         }
     }
 }
--- prolific.collector/test/collector.test.js
+++ prolific.collector/test/collector.test.js
@@ -4,45 +4,51 @@ describe('collector', () => {
     it('can order synchronous messages', () => {
         const collector = new Collector
         const test = []
-        collector.on('data', data => test.push(data.name))
+        collector.on('data', data => test.push(data.body.method))
         collector.on('notice', message => test.push(message.label))
         collector.data({
             start: 0,
             series: 1,
             when: 0,
-            path: [ 1, 2 ],
-            name: 'exit'
+            pid: 2,
+            body: { method: 'exit' }
         })
         collector.data({
             start: 0,
             series: 0,
             when: 0,
-            path: [ 1, 2 ],
-            name: 'start'
+            pid: 2,
+            body: { method: 'start' }
         })
-        collector.exit(2)
-        assert.deepStrictEqual(test, [ 'start', 'exit' ], 'ordered')
+        collector.data({
+            pid: 2,
+            body: { method: 'eos' }
+        })
+        assert.deepStrictEqual(test, [ 'start', 'exit', 'eos' ], 'ordered')
     })
     it('can exit with missing messages', () => {
         const collector = new Collector
         const test = []
-        collector.on('data', data => test.push(data.name))
+        collector.on('data', data => test.push(data.body.method))
         collector.on('notice', message => test.push(message.label))
         collector.data({
             start: 0,
             series: 0,
             when: 0,
-            path: [ 1, 2 ],
-            name: 'start'
+            pid: 2,
+            body: { method: 'start' }
         })
         collector.data({
             start: 0,
             series: 2,
             when: 0,
-            path: [ 1, 2 ],
-            name: 'exit'
+            pid: 2,
+            body: { method: 'exit' }
         })
-        collector.exit(2)
-        assert.deepStrictEqual(test, [ 'start', 'gap' ], 'ordered')
+        collector.data({
+            pid: 2,
+            body: { method: 'eos' }
+        })
+        assert.deepStrictEqual(test, [ 'start', 'gap', 'eos' ], 'ordered')
     })
 })
