e88d431b8c5aa12ae72da31df4f492d02f05717d
Alan Gutierrez <alan@prettyrobots.com>
2019-09-17 02:07:15 -0500

    Create a printer that will write to standard out.
    See #1121.

---
+++ prolific.executable/printer.js
@@ -0,0 +1,40 @@
+const ascension = require('ascension')
+const sort = ascension([ Number ], entry => entry.when)
+
+const Isochronous = require('isochronous')
+
+class Printer {
+    constructor (destructible, write, format, interval) {
+        this._write = write
+        this._entries = []
+        this._format = format
+        const isochronous = new Isochronous(interval, true, this._check.bind(this))
+        destructible.durable('isochronous', isochronous.start())
+        destructible.destruct(() => isochronous.stop())
+        destructible.destruct(() => this._log(this._entries.splice(0)))
+    }
+
+    push (envelope) {
+        this._entries.push.apply(this._entries, envelope.entries)
+    }
+
+    _log (entries) {
+        if (entries.length != 0) {
+            this._write.call(null, entries.map(this._format).join('\n'))
+        }
+    }
+
+    _check (status) {
+        this._entries.sort(sort)
+        const stop = status.scheduled - Math.ceil(status.interval * 1.51)
+        let i, I
+        for (i = 0, I = this._entries.length; i < I; i++) {
+            if (this._entries[i].when > stop) {
+                break
+            }
+        }
+        this._log(this._entries.splice(0, i))
+    }
+}
+
+module.exports = Printer
---
+++ prolific.executable/test/printer.test.js
@@ -0,0 +1,23 @@
+describe('printer', () => {
+    const assert = require('assert')
+    const Printer = require('../printer')
+    it('can print', async () => {
+        const test = []
+        const Destructible = require('destructible')
+        const destructible = new Destructible(__filename)
+        const latch = {}
+        latch.unlatched = new Promise(resolve => latch.unlatch = resolve)
+        const printer = new Printer(destructible.durable('printer'), lines => {
+            test.push(lines)
+            latch.unlatch.call()
+        }, JSON.stringify, 100)
+        const now = Date.now()
+        printer.push({ entries: [{ when: now, a: 1 }] })
+        printer.push({ entries: [{ when: now, a: 1 }] })
+        await latch.unlatched
+        destructible.destroy()
+        await destructible.promise
+        assert.equal(test.length, 1, 'printed')
+        assert.equal(test[0].split('\n').length, 2, 'batched')
+    })
+})
