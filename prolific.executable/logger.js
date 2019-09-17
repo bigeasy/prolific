10c1a8abe1a002c0370f08bc7f47b3c5042f5dad
Alan Gutierrez <alan@prettyrobots.com>
2019-09-17 01:06:00 -0500

    Create an interal prolific Logger.
    The logger writes to the temporary directory. We'll use the watcher to
    print the logger output to standard out.
    
    See #1121.

---
+++ prolific.executable/logger.js
@@ -0,0 +1,26 @@
+const Isochronous = require('isochronous')
+const Publisher = require('prolific.queue/publisher')
+
+class Logger {
+    constructor (destructible, Date, directory, pid, interval) {
+        this._entries = []
+        this._publisher = new Publisher(Date, directory, pid)
+        this._Date = Date
+        const isochornous = new Isochronous(interval, true, this._flush.bind(this))
+        destructible.durable('interval', isochornous.start())
+        destructible.destruct(() => isochornous.stop())
+        destructible.destruct(() => this._flush())
+    }
+
+    push (entry) {
+        this._entries.push({ when: this._Date.now(), ...entry })
+    }
+
+    _flush () {
+        if (this._entries.length != 0) {
+            this._publisher.publish({ method: 'log', entries: this._entries.splice(0) })
+        }
+    }
+}
+
+module.exports = Logger
---
+++ prolific.executable/test/logger.test.js
@@ -0,0 +1,40 @@
+describe('logger', () => {
+    const fs = require('fs').promises
+    const assert = require('assert')
+    const path = require('path')
+
+    const rimraf = require('rimraf')
+    const callback = require('prospective/callback')
+    const once = require('prospective/once')
+
+    const Watcher = require('prolific.watcher')
+
+
+    const Logger = require('../logger')
+
+    const TMPDIR = path.join(__dirname, 'tmp')
+    const fnv = require('hash.fnv')
+    const dir = {
+        stage: path.resolve(TMPDIR, 'stage'),
+        publish: path.resolve(TMPDIR, 'publish')
+    }
+    it('can write log entries', async () => {
+        await callback(callback => rimraf(TMPDIR, callback))
+        await fs.mkdir(dir.publish, { recursive: true })
+        await fs.mkdir(dir.stage, { recursive: true })
+        const Destructible = require('destructible')
+        const destructible = new Destructible(__filename)
+        const watcher = new Watcher(destructible, buffer => {
+            return fnv(0, buffer, 0, buffer.length)
+        }, path.join(TMPDIR, 'publish'))
+        const data = once(watcher, 'data').promise
+        const logger = new Logger(destructible.durable('logger'), {
+            now: () => 0
+        }, TMPDIR, 2, 100)
+        logger.push({ a: 1 })
+        const [ log ] = await data
+        assert.deepStrictEqual(log.body.entries, [ { when: 0, a: 1 } ])
+        destructible.destroy()
+        await destructible.promise
+    })
+})
