70499718a1e60300e4a4597201943864ba73c4c5
Alan Gutierrez <alan@prettyrobots.com>
2019-09-18 09:04:44 -0500

    Use `Destructible` to decrement `Descendent`.
    Remove `try`/`catch` from sidecar.
    
    See #1125.

--- prolific.executable/sidecar.bin.js
+++ prolific.executable/sidecar.bin.js
@@ -40,68 +40,66 @@ require('arguable')(module, {
 
     descendant.process = arguable.options.process
     descendant.increment()
-    try {
-        const _logger = require('prolific.logger').create('prolific')
-        function memoryUsage () { _logger.notice('memory', process.memoryUsage()) }
-        memoryUsage()
-        const interval = setInterval(memoryUsage, 1000)
-        destructible.destruct(() => clearInterval(interval))
+    destructible.destruct(() => descendant.decrement())
+    destructible.destruct(() => descendant.process = process)
+    const _logger = require('prolific.logger').create('prolific')
+    function memoryUsage () { _logger.notice('memory', process.memoryUsage()) }
+    memoryUsage()
+    const interval = setInterval(memoryUsage, 1000)
+    destructible.destruct(() => clearInterval(interval))
 
-        const Processor = require('./processor')
+    const Processor = require('./processor')
 
-        const processor = new Processor(arguable.ultimate.configuration)
+    const processor = new Processor(arguable.ultimate.configuration)
 
-        const Queue = require('avenue')
-        const queue = new Queue
+    const Queue = require('avenue')
+    const queue = new Queue
 
-        const processors = new Queue().shifter().paired
+    const processors = new Queue().shifter().paired
 
-        const Future = require('prospective/future')
+    const Future = require('prospective/future')
 
-        async function update (socket) {
-            for await (const processor of processors.shifter.iterator()) {
-                logger.say('processor.send', { processor })
-                socket.write(JSON.stringify(processor) + '\n')
-            }
+    async function update (socket) {
+        for await (const processor of processors.shifter.iterator()) {
+            logger.say('processor.send', { processor })
+            socket.write(JSON.stringify(processor) + '\n')
         }
-
-        descendant.on('prolific:socket', (message, socket) => {
-            logger.say('sidecar.socket', { message, socket: !! socket, connected: process.connected })
-            destructible.ephemeral('read', update(socket))
-            destructible.ephemeral('asynchronous', consolidator.asynchronous(socket, socket))
-        })
-
-        destructible.destruct(() => processors.shifter.destroy())
-
-        processor.on('configuration', (processor) => {
-            logger.say('processor.load', { processor })
-            processors.queue.push(processor)
-        })
-
-        // Listen to our asynchronous pipe.
-        const consolidator = new Consolidator(queue)
-
-        destructible.durable('process', queue.shifter().pump(chunk => {
-            logger.say('processor.chunk', { chunk })
-            processor.process(chunk)
-        }))
-        destructible.ephemeral('configure', processor.configure())
-
-        descendant.on('prolific:synchronous', synchronous => {
-            consolidator.synchronous(synchronous.body)
-        })
-
-        // Let the supervisor know that we're ready. It will send our asynchronous
-        // pipe down to the monitored process.
-        descendant.up(+arguable.ultimate.supervisor, 'prolific:receiving', process.pid)
-
-        await destructible.promise
-
-        return 0
-    } finally {
-        descendant.decrement()
-        process.removeListener('message', dump)
-        process.removeListener('internalMessage', dump)
-        descendant.process = process
     }
+
+    descendant.on('prolific:socket', (message, socket) => {
+        logger.say('sidecar.socket', { message, socket: !! socket, connected: process.connected })
+        destructible.ephemeral('read', update(socket))
+        destructible.ephemeral('asynchronous', consolidator.asynchronous(socket, socket))
+    })
+
+    destructible.destruct(() => processors.shifter.destroy())
+
+    processor.on('configuration', (processor) => {
+        logger.say('processor.load', { processor })
+        processors.queue.push(processor)
+    })
+
+    // Listen to our asynchronous pipe.
+    const consolidator = new Consolidator(queue)
+
+    destructible.durable('process', queue.shifter().pump(chunk => {
+        logger.say('processor.chunk', { chunk })
+        processor.process(chunk)
+    }))
+    destructible.ephemeral('configure', processor.configure())
+
+    descendant.on('prolific:synchronous', synchronous => {
+        consolidator.synchronous(synchronous.body)
+    })
+
+    // Let the supervisor know that we're ready. It will send our asynchronous
+    // pipe down to the monitored process.
+    descendant.up(+arguable.ultimate.supervisor, 'prolific:receiving', process.pid)
+
+    await destructible.promise
+
+    process.removeListener('message', dump)
+    process.removeListener('internalMessage', dump)
+
+    return 0
 })
