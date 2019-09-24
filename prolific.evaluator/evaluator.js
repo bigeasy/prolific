0655996ba22d63a59267ff56193e81cb5b8c63e4
Alan Gutierrez <alan@prettyrobots.com>
2019-09-24 06:03:35 -0500

    Fix for Node.js rename of `createRequire`.
    `createRequire` was added in Node.js 12. A new function that uses either
    a file path or a `URL` object. It is missing in Node.js 10. We have to
    probe to see what's available.
    
    Closes #1141.

--- prolific.evaluator/evaluator.js
+++ prolific.evaluator/evaluator.js
@@ -1,12 +1,14 @@
+const coalesce = require('extant')
 const assert = require('assert')
 const path = require('path')
-const { createRequire } = require('module')
+const _module = require('module')
 
 exports.create = function (source, file, relative) {
     const resolved = path.resolve(file)
     const loader = new Function('processor', 'require', '__dirname', '__filename', source)
     const processor = {}
-    const _require = createRequire(path.resolve(relative))
+    const createRequire = coalesce(_module.createRequire, _module.createRequireFromPath)
+    const _require = createRequire.call(_module, path.resolve(relative))
     loader(processor, _require, path.dirname(resolved), resolved)
     assert(typeof processor.triage == 'function')
     assert(typeof processor.process == 'function')
