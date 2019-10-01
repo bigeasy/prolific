ef1dd8253d38fb16a28003590bafc21137a31837
Alan Gutierrez <alan@prettyrobots.com>
2019-10-01 12:47:40 -0500

    Revert Prolific Sink test to Proof.
    See #1198.

--- prolific.sink/test/sink.test.js
+++ prolific.sink/test/sink.test.js
@@ -1,27 +1,24 @@
-describe('sink', () => {
-    const assert = require('assert')
-    it('can record JSON', () => {
-        const sink = require('..')
-        sink.Date = { now: function () { return 0 } }
-        sink.properties = { z: 26, when: 0 }
-        sink.json('error', 'hello', 'greeting', { a: 1, when: 0 })
-        sink.queue = []
-        sink.json = function (level, qualifier, label, body) {
-            this.queue.push({
-                level: level,
-                qualifier: qualifier,
-                label: label,
-                body: body,
-                system: this.properties
-            })
-        }
-        sink.json('error', 'hello', 'greeting', { a: 1, when: 0 })
-        assert.deepStrictEqual(sink.queue, [{
-            level: 'error',
-            qualifier: 'hello',
-            label: 'greeting',
-            body: { a: 1, when: 0 },
-            system: { z: 26, when: 0 }
-        }], 'json')
-    })
+require('proof')(1, (okay) => {
+    const sink = require('..')
+    sink.Date = { now: function () { return 0 } }
+    sink.properties = { z: 26, when: 0 }
+    sink.json('error', 'hello', 'greeting', { a: 1, when: 0 })
+    sink.queue = []
+    sink.json = function (level, qualifier, label, body) {
+        this.queue.push({
+            level: level,
+            qualifier: qualifier,
+            label: label,
+            body: body,
+            system: this.properties
+        })
+    }
+    sink.json('error', 'hello', 'greeting', { a: 1, when: 0 })
+    okay(sink.queue, [{
+        level: 'error',
+        qualifier: 'hello',
+        label: 'greeting',
+        body: { a: 1, when: 0 },
+        system: { z: 26, when: 0 }
+    }], 'json')
 })
