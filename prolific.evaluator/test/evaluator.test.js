9f11c79efb5a7e388205f4a817a1b931561e6637
Alan Gutierrez <alan@prettyrobots.com>
2019-10-03 00:32:40 -0500

    Revert Prolific Evaluator tests to Proof.
    See #1198.

--- prolific.evaluator/test/evaluator.test.js
+++ prolific.evaluator/test/evaluator.test.js
@@ -1,29 +1,28 @@
-describe('evaluator', () => {
-    const assert = require('assert')
+require('proof')(4, async (okay) => {
     const Evaluator = require('../evaluator')
     const path = require('path')
     const fs = require('fs').promises
-    it('can load a processor', async () => {
+    {
         const file = path.join(__dirname, 'processor.js')
         const source = await fs.readFile(file, 'utf8')
         const resolved = Evaluator.resolve(file, path.resolve(__dirname, '..', 'evaluator.js'))
 
         const processor = Evaluator.create(source, resolved)
         const triage = await processor.triage()
-        assert(!triage(1), 'triage skip')
-        assert(triage(0), 'triage hit')
+        okay(!triage(1), 'triage skip')
+        okay(triage(0), 'triage hit')
         const process = await processor.process()
-        assert.deepStrictEqual(await process(), {
+        okay(await process(), {
             other: 1,
             __dirname: path.resolve(__dirname, '..'),
             __filename: path.resolve(__dirname, '..', 'processor.js')
         }, 'processor')
-    })
-    it('can resolve a processor module', () => {
+    }
+    {
         const resolved = Evaluator.resolve('./test/processor', path.resolve(__dirname, '..', 'evaluator.js'))
-        assert.deepStrictEqual(resolved, {
+        okay(resolved, {
             filename: path.resolve(__dirname, './processor.js'),
             __filename: path.resolve(__dirname, './processor.js')
         }, 'resolved')
-    })
+    }
 })
