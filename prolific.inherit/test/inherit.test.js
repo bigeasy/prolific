7730aab532b091e8e7ab4b5531f55ec00e18a62a
Alan Gutierrez <alan@prettyrobots.com>
2019-10-03 00:08:17 -0500

    Revert Prolific Inherit to Proof.
    See #1198.

--- prolific.inherit/test/inherit.test.js
+++ prolific.inherit/test/inherit.test.js
@@ -1,11 +1,8 @@
-describe('inherit', () => {
-    const assert = require('assert')
+require('proof')(1, (okay) => {
     const inherit = require('../inherit')
-    it('can create a stdio definition', () => {
-        const inheritance = inherit({
-            env: { INHERIT_FD: 5 },
-            arrayed: { inherit: [ 0, 1, 2, 4, 4, 'INHERIT_FD', 8 ] }
-        })
-        assert.deepStrictEqual(inheritance, [ 0, 1, 2, 'ignore', 4, 5, 'ignore', 'ignore', 8 ], 'inherit')
+    const inheritance = inherit({
+        env: { INHERIT_FD: 5 },
+        arrayed: { inherit: [ 0, 1, 2, 4, 4, 'INHERIT_FD', 8 ] }
     })
+    okay(inheritance, [ 0, 1, 2, 'ignore', 4, 5, 'ignore', 'ignore', 8 ], 'inherit')
 })
