90c1204256eea02da89fae9eb2bbae62d5823d93
Alan Gutierrez <alan@prettyrobots.com>
2019-10-03 00:39:38 -0500

    Revert Prolific Logger tests to Proof.
    See #1198.

--- prolific.logger/test/logger.test.js
+++ prolific.logger/test/logger.test.js
@@ -1,52 +1,49 @@
-describe('logger', () => {
-    const assert = require('assert')
+require('proof')(1, (okay) => {
     const Logger = require('..')
-    it('can log', () => {
-        const test = []
-        Logger.sink.json = function (...vargs) {
-            test.push(vargs)
-        }
-        const logger = Logger.create('hello')
-        logger.concat('error', 'greeting', {}, { a: 1 })
-        logger.log('error', 'greeting', { a: 1 })
-        logger.error('greeting', { a: 1 })
-        const expect = [{
-            vargs: [
-                'panic',
-                'hello',
-                'exception',
-                { code: 'EOUCH', key: 'value', stack: false },
-                { pid: process.pid }
-            ],
-            message: 'level, context, properties'
-        }, {
-            vargs: [
-                'error',
-                'hello',
-                'exception',
-                { stack: true, code: 'EOUCH' },
-                { pid: process.pid }
-            ],
-            message: 'no level nor context nor properties'
-        }]
-        Logger.sink.json = function (...vargs) {
-            test.push(vargs)
-        }
-        const error = new Error('ouch')
-        error.code = 'EOUCH'
-        logger.stackTrace('panic', 'exception', { key: 'value' }, [ 'code', 'missing' ])(error)
-        logger.stackTrace('exception')(error)
-        test[test.length - 1][3].stack = !! test[test.length - 1][3].stack
-        assert.deepStrictEqual(test, [[
-            'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
-        ], [
-            'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
-        ], [
-            'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
-        ], [
-            'panic', 'hello', 'exception', { code: 'EOUCH', key: 'value' }, { pid: process.pid }
-        ], [
-            'error', 'hello', 'exception', { code: 'EOUCH', stack: true }, { pid: process.pid }
-        ]], 'test')
-    })
+    const test = []
+    Logger.sink.json = function (...vargs) {
+        test.push(vargs)
+    }
+    const logger = Logger.create('hello')
+    logger.concat('error', 'greeting', {}, { a: 1 })
+    logger.log('error', 'greeting', { a: 1 })
+    logger.error('greeting', { a: 1 })
+    const expect = [{
+        vargs: [
+            'panic',
+            'hello',
+            'exception',
+            { code: 'EOUCH', key: 'value', stack: false },
+            { pid: process.pid }
+        ],
+        message: 'level, context, properties'
+    }, {
+        vargs: [
+            'error',
+            'hello',
+            'exception',
+            { stack: true, code: 'EOUCH' },
+            { pid: process.pid }
+        ],
+        message: 'no level nor context nor properties'
+    }]
+    Logger.sink.json = function (...vargs) {
+        test.push(vargs)
+    }
+    const error = new Error('ouch')
+    error.code = 'EOUCH'
+    logger.stackTrace('panic', 'exception', { key: 'value' }, [ 'code', 'missing' ])(error)
+    logger.stackTrace('exception')(error)
+    test[test.length - 1][3].stack = !! test[test.length - 1][3].stack
+    okay(test, [[
+        'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
+    ], [
+        'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
+    ], [
+        'error', 'hello', 'greeting', { a: 1 }, { pid: process.pid }
+    ], [
+        'panic', 'hello', 'exception', { code: 'EOUCH', key: 'value' }, { pid: process.pid }
+    ], [
+        'error', 'hello', 'exception', { code: 'EOUCH', stack: true }, { pid: process.pid }
+    ]], 'test')
 })
