e0f39ecc04c7c342205ea4bdf70e8b18997bb08f
Alan Gutierrez <alan@prettyrobots.com>
2019-12-13 11:07:44 -0600

    Ignore final line if missing newline.
    Crashing when the async stream is truncated leaving a partial line of
    JSON at the end of the stream. Instead of splitting lines now, we
    extract a line at a time looking for the newline.
    
    See #1246.

--- prolific.consolidator/consolidator.js
+++ prolific.consolidator/consolidator.js
@@ -4,27 +4,53 @@ const Staccato = require('staccato')
 const events = require('events')
 
 class Consolidator extends events.EventEmitter {
-    constructor (queue) {
+    constructor (queue, logger) {
         super()
         this._eos = false
         this._series = 0
         this._queue = queue
         this._readable = { destroy: () => {} }
         this._writable = { destroy: () => {} }
+        this._logger = logger
     }
 
     async asynchronous (input, output) {
-        this._readable = new Staccato.Readable(byline(input))
+        this._readable = new Staccato.Readable(input)
         this._writable = new Staccato.Writable(output)
-        for await (const line of this._readable) {
-            const json = JSON.parse(line.toString())
-            assert.equal(json.series, this._series, 'series mismatch')
-            this._series = (this._series + 1) & 0xffffff
-            this._queue.push(json)
-            await this._writable.write(JSON.stringify({
-                method: 'receipt',
-                series: json.series
-            }) + '\n')
+        let remainder = Buffer.alloc(0)
+        for await (const chunk of this._readable) {
+            const buffer = remainder.length == 0 ? chunk : Buffer.concat([ remainder, chunk ])
+            let start = 0
+            for (;;) {
+                const end = buffer.indexOf(0xa, start)
+                if (~end) {
+                    const line = buffer.slice(start, end).toString()
+                    try {
+                        const json = JSON.parse(line)
+                        if (json.series == this._series) {
+                            this._series = (this._series + 1) & 0xffffff
+                        } else {
+                            this._logger.say('consolidator.series', {
+                                expected: this._series,
+                                actual: json.series
+                            })
+                            this._series = json.series + 1
+                        }
+                        this._queue.push(json)
+                        await this._writable.write(JSON.stringify({
+                            method: 'receipt',
+                            series: json.series
+                        }) + '\n')
+                    } catch (error) {
+                        assert(error instanceof SyntaxError)
+                        this._logger.say('consolidator.json', { line })
+                    }
+                    start = end + 1
+                } else {
+                    remainder = buffer.slice(start)
+                    break
+                }
+            }
         }
     }
 
